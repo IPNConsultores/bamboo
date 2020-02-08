7983fe3ab02edc7f35117eecd9656990f2d2b680
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-08 19:04:06 -0300

    feabarcas v1.12

--- backend/crea_cliente.php
+++ backend/crea_cliente.php
@@ -9,6 +9,8 @@
  $email=estandariza_info(_GET["email"]);
  $direccion=estandariza_info($_GET["direccion"]);
  //$telefono=estandariza_info($_GET["telefono"]);
+ echo "nombre= ".$nombre;
+ echo "direccion= ".$direccion;
 $dbhost = 'localhost';
 $dbname = 'asesori1_bamboo';
 $dbuser = 'asesori1_cesnar';
@@ -18,9 +20,9 @@ $st=mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if ($st){
 mysqli_set_charset( $st, 'utf8');
 mysqli_select_db($st, $dbname);
-mysqli_query($st, 'insert into clientes(nombre_cliente, apellido_paterno, apellido_materno, rut_sin_dv, dv, direccion_personal, correo) values 
-(\''.$nombre.'\', \''.$apellidop.'\', \''.$apellidom.'\', \''.$rut.'\', \''.$dv.'\', \''.$direccion.'\', \''.$email.'\');');
-echo "conectado al servidor;\n";
+//mysqli_query($st, 'insert into clientes(nombre_cliente, apellido_paterno, apellido_materno, rut_sin_dv, dv, direccion_personal, correo) values (\''.$nombre.'\', \''.$apellidop.'\', \''.$apellidom.'\', \''.$rut.'\', \''.$dv.'\', \''.$direccion.'\', \''.$email.'\');');
+
+echo 'insert into clientes(nombre_cliente, apellido_paterno, apellido_materno, rut_sin_dv, dv, direccion_personal, correo) values (\''.$nombre.'\', \''.$apellidop.'\', \''.$apellidom.'\', \''.$rut.'\', \''.$dv.'\', \''.$direccion.'\', \''.$email.'\');';
 }
 else{
 echo "no conectado al servidor;\n";
@@ -32,6 +34,3 @@ function estandariza_info($data) {
     return $data;
   }
 ?>
-
-
-http://ipnconsultores.cl/bamboo/backend/crea_cliente.php?nombre=felipe&apellidop=abarca&apellidom=soto&rut=17029236&dv=7&email=fabarca212%40gmail.com&direccion=santa+isabel+1610
