4c2b926fc6466bb6af2903f303c6f49f049f26fd
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-08 18:52:25 -0300

    feabarcas v1.1

--- backend/crea_cliente.php
+++ backend/crea_cliente.php
@@ -1,6 +1,14 @@
 <?php
-//update listado_licitaciones as a left join (SELECT codigo_externo, max(fecha_creacion) as MAXFECHA FROM `listado_licitaciones` group by codigo_externo) as b on a.codigo_externo=b.codigo_externo set antigua=1 where a.fecha_creacion<>b.MAXFECHA
+//update listado_licitaciones as a left join (SELECT codigo_externo, max(fecha_creacion) as MAXFECHA FROM listado_licitaciones group by codigo_externo) as b on a.codigo_externo=b.codigo_externo set antigua=1 where a.fecha_creacion<>b.MAXFECHA
  //$dias=$argv[1];
+ $nombre=estandariza_info($_GET["nombre"]);
+ $apellidop=estandariza_info($_GET["apellidop"]);
+ $apellidom=estandariza_info($_GET["apellidom"]);
+ $rut=estandariza_info($_GET["rut"]);
+ $dv=estandariza_info($_GET["dv"]);
+ $email=$estandariza_info(_GET["email"]);
+ $direccion=estandariza_info($_GET["direccion"]);
+ //$telefono=estandariza_info($_GET["telefono"]);
 $dbhost = 'localhost';
 $dbname = 'asesori1_bamboo';
 $dbuser = 'asesori1_cesnar';
@@ -10,17 +18,17 @@ $st=mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if ($st){
 mysqli_set_charset( $st, 'utf8');
 mysqli_select_db($st, $dbname);
-//$limpia = mysqli_query($st, 'select * from clientes;');
+mysqli_query($st, 'insert into clientes(nombre_cliente, apellido_paterno, apellido_materno, rut_sin_dv, dv, direccion_personal, correo) values 
+(\''.$nombre.'\', \''.$apellidop.'\', \''.$apellidom.'\', \''.$rut.'\', \''.$dv.'\', \''.$direccion.'\', \''.$email.'\');');
 echo "conectado al servidor;\n";
 }
 else{
 echo "no conectado al servidor;\n";
 }
-echo $_GET["nombre"]."\n";
-echo $_GET["apellidop"]."\n";
-echo $_GET["apellidom"]."\n";
-echo $_GET["rut"]."\n";
-echo $_GET["dv"]."\n";
-echo $_GET["email"]."\n";
-echo $_GET["direccion"]."\n";
+function estandariza_info($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
 ?>
