b69b0dde0d944e832c1ba429ce309ce1ad0ec72a
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-09 00:27:13 -0300

    feabarcas v1.34

--- test.php
+++ test.php
@@ -242,7 +242,7 @@ body {
           <div class="form-group">
             <button type="submit" class="btn btn-primary btn-block btn-lg" value="Login" style= "box-shadow: 5; overflow:auto;  background-color: #A5CCAB; color: #536656">Ingresar</button>
           </div>
-          <p>No tienes cuenta? <a href="registro.php">Regístrate</a>.</p>
+          <p>No tienes cuenta? <a href="/bamboo/backend/registro.php">Regístrate</a>.</p>
           <p class="hint-text"><a href="#" style="color:#536656">¿Olvidaste contraseña?</a></p>
         </form>
       </div>
