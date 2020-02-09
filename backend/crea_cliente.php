0f6e1000a23263926a995fd0bb2ae72b3dd60807
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-09 16:24:36 -0300

    feabarcas v1.63

--- backend/crea_cliente.php
+++ backend/crea_cliente.php
@@ -19,5 +19,5 @@ function estandariza_info($data) {
     $data = htmlspecialchars($data);
     return $data;
   }
-  header("Location:http://ipnconsultores.cl/bamboo/index.php");
+//  header("Location:http://ipnconsultores.cl/bamboo/index.php");
 ?>
