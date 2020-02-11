05bd4270272b2ef682e6642601df99c0ed1c27d8
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-10 22:04:53 -0300

    feabarcas v1.74

--- index.php
+++ index.php
@@ -70,32 +70,7 @@ function abrir_creacion_cliente() {
        $( "#load" ).load( "Creación_ClienteV3.html" );}
 function abrir_modif_cliente() {
        $( "#load" ).load( "listado_clientes.php" );}
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-
 </script> 
-<!-- Include all compiled plugins (below), or include individual files as needed --> 
-<script src="js/popper.min.js"></script> 
-<script src="js/bootstrap-4.3.1.js"></script> 
-<script>
-	(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-
-	</script>
 </body>
 </html>
 
