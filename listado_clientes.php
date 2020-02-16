e621f27f8f7e084af4384df9815e87de6d9d05c7
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-16 20:58:20 -0300

    listado

--- listado_clientes.php
+++ listado_clientes.php
@@ -70,7 +70,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!--    <link href="css/bootstrap-4.3.1.css" rel="stylesheet">-->
+    <link href="css/bootstrap-4.3.1.css" rel="stylesheet">
     <title>Modificación Cliente</title>
     <!-- Bootstrap -->
 </head>
