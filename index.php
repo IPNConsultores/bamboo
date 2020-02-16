018e476f8d4e70dd886ae5aec412082120431e64
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-15 21:41:03 -0300

    feabarcas v1.66

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 
 <body>
-    <div id="header"><?php include 'header.php' ?></div>
+    <div id="header" data-source="header.php"></div>
     <div id="contenido_index">
         <p>Index</p>
     </div>
