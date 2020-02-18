c58f9054d33642458498fc25390420a6ccb0f9ab
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-18 19:56:39 -0300

    feabarcas v2.01

--- header.php
+++ header.php
@@ -75,7 +75,7 @@ if ( !isset( $_SESSION[ "loggedin" ] ) || $_SESSION[ "loggedin" ] !== true ) {
                 <form class="form-inline" method="POST" action="/bamboo/listado_clientes.php">
                     <input class="form-control" type="text" placeholder="Buscar" aria-label="Buscar">
                     <button class="btn btn-outline-success " id="busqueda_header" name="busqueda_header" type="submit"
-                        style="background-color:#536656;color:#A5CCAB ">Buscar</button>
+                        style="background-color:#536656;color:#A5CCAB " action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" >Buscar</button>
                 </form>
             </ul>
         </nav>
