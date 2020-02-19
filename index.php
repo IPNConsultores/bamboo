d26ba356816d8ef662607e5405e396d3be136eaf
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-19 19:24:47 -0300

    feabarcas v2.20

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!--    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">-->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <link href="css/bootstrap-4.3.1.css" rel="stylesheet">
@@ -13,7 +13,7 @@
 </head>
 
 <body>
-    <div id="header"><?php include 'header2.php' ?></div>
+    <div id="header"><?php include 'header.php' ?></div>
     <div id="contenido_index">
         <p>Index</p>
     </div>
