c99707445484f6af7e77293cfc0d701fe5b72efb
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-18 22:36:42 -0300

    headder v2.5

--- header2.php
+++ header2.php
@@ -79,7 +79,11 @@ if ( !isset( $_SESSION[ "loggedin" ] ) || $_SESSION[ "loggedin" ] !== true ) {
 <script src="js/jquery-3.3.1.min.js"></script> 
 
 <!-- Include all compiled plugins (below), or include individual files as needed --> 
+<!--
 <script src="js/popper.min.js"></script> 
 <script src="js/bootstrap-4.3.1.js"></script>
+-->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 </body>
 </html>
