4440161084dc1f862e72f23567be1dd0790f67e1
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-24 22:36:04 -0300

    feabarcas v2.30

--- bamboo/header2.php
+++ bamboo/header2.php
@@ -1,14 +1,13 @@
 <?php
 // Initialize the session
 session_start();
-
+$_SESSION["auxiliar"]=false;
 // Check if the user is logged in, if not then redirect him to login page
 if ( !isset( $_SESSION[ "loggedin" ] ) || $_SESSION[ "loggedin" ] !== true ) {
   header( "location: /backend/login/login.php" );
   exit;
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
--- bamboo/listado_clientes.php
+++ bamboo/listado_clientes.php
@@ -1,5 +1,5 @@
 <?php
-//session_start();
+session_start();
 function estandariza_info($data) {
     $data = trim($data);
     $data = stripslashes($data);
