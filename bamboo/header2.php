d09fae31886a4718cac4b12dc5cb45d0faade475
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-24 21:28:26 -0300

    v3

--- bamboo/header2.php
+++ bamboo/header2.php
@@ -91,7 +91,7 @@ if ( !isset( $_SESSION[ "loggedin" ] ) || $_SESSION[ "loggedin" ] !== true ) {
         </form>
     </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery-3.3.1.min.js"></script>
+<!--    <script src="js/jquery-3.3.1.min.js"></script>-->
 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <!--
