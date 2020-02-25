7344891168615f15eaa482061b8dd26cecb37215
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-24 22:27:38 -0300

    header js

--- bamboo/header2.php
+++ bamboo/header2.php
@@ -71,7 +71,7 @@ if ( !isset( $_SESSION[ "loggedin" ] ) || $_SESSION[ "loggedin" ] !== true ) {
         }
 	</script> 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery-3.3.1.min.js"></script>
+<!--    <script src="js/jquery-3.3.1.min.js"></script>-->
 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <!--
