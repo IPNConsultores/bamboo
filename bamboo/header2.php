58720b3e5b7f329cbaebc2166e56480f004c83fe
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-24 21:48:42 -0300

    feabarcas v2.29

--- bamboo/header2.php
+++ bamboo/header2.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if the user is logged in, if not then redirect him to login page
 if ( !isset( $_SESSION[ "loggedin" ] ) || $_SESSION[ "loggedin" ] !== true ) {
-  header( "location: /bamboo/backend/login.php" );
+  header( "location: /backend/login/login.php" );
   exit;
 }
 ?>
