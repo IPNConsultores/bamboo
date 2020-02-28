32da5931a10fa949f01f1ce5bfb4913262298889
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-27 22:07:42 -0300

    feabarcas v2.49

--- backend/login/login.php
+++ backend/login/login.php
@@ -1,7 +1,7 @@
 <?php
 // Initialize the session
 session_start();
- 
+print_r($_SESSION);
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: /bamboo/index.php");
