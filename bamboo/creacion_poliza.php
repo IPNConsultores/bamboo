d8ebc2ea00c88149699d3e786e5c7ed1b62ecdb9
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-29 14:57:51 -0300

    Creacion_Poliza

--- bamboo/creacion_poliza.php
+++ bamboo/creacion_poliza.php
@@ -6,8 +6,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Bamboo Seguros</title>
 <!-- Bootstrap -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
@@ -16,79 +19,26 @@
 <?php include 'header2.php' ?>
 </div>
 <div class="container">
-<p>Póliza / Creación<br>
-</p>
-<h5 class="form-row">&nbsp;Datos Póliza</h5>
-<div class="accordion" id="accordionExample">
-  <div class="card" >
-    <div class="card-header" id="headingOne" style="background-color:whitesmoke">
-      <h5 class="mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="color:#536656">Asegurado y Proponente</button>
-      </h5>
-    </div>
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-        <p>Datos Proponente<br>
-        <div class="form-row">
-                   <div class="form-row">
-                        <div class="col-md mb-3">
-                            <label for="RUT">RUT</label>
-                            <input type="text" class="form-control" id="rutprop" name="rutprop" placeholder="1111111-1"
-                            oninput="checkRut(this)" required>
-                            <script>
-                            function valida_rut() {
-                                
-                                var dato = $('#rut').val();
-                                /*
-                                var r = confirm(
-                                    "El rut que acabas de ingresar ya se encuentra en la base de datos. ¿Deseas ver la información asociada al rut?"
-                                );
-                                if (r == true) {
-                                    $.redirect('/bamboo/listado_clientes.php', {
-                                        'dato': dato
-                                    }, 'post');
-                                } else {
-                                    location.href = "http://gestionipn.cl/bamboo/creacion_cliente.php";
-                                }
-                                */
-                            }
-                            </script>
-                            <div class="invalid-feedback"> Dígito verificador no válido. Verifica rut ingresado </div>
-                        </div>
-                      
-                    </div>
-          
-          <div class="col-md-2 mb-3 col-xl-3 col-lg-1 offset-lg-0">
-            <label for="prop">&nbsp;</label>
-            <br>
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="background-color:#536656;color:#A5CCAB;position:inherit">Buscar</button>
-          </div>
+  <p>Póliza / Creación<br>
+  </p>
+  <h5 class="form-row">&nbsp;Datos Póliza</h5>
+  <div class="accordion" id="accordionExample">
+    <div class="card" >
+      <div class="card-header" id="headingOne" style="background-color:whitesmoke">
+        <h5 class="mb-0">
+          <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="color:#536656">Asegurado y Proponente</button>
+        </h5>
+      </div>
+      <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+        <div class="card-body">
+          <p>Datos Proponente<br>
           <div class="form-row">
-            <div class="col-md-4 mb-3">
-              <label for="Nombre">Nombre</label>
-              <input type="text" class="form-control" name="nombre" required>
-              <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-            </div>
-            <div class="col-md-4 mb-3">
-              <label for="ApellidoP">Apellido Paterno</label>
-              <input type="text" class="form-control" name="apellidop" required>
-              <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-            </div>
-            <div class="col-md-4 mb-3">
-              <label for="ApellidoM">Apellido Materno</label>
-              <input type="text" class="form-control" name="apellidom" required>
-              <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-            </div>
-          </div>
-        </div>
-        <p>Datos Asegurado<br>
             <div class="form-row">
-                   <div class="form-row">
-                        <div class="col-md mb-3">
-                            <label for="RUT">RUT</label>
-                            <input type="text" class="form-control" id="rutaseg" name="rutaseg" placeholder="1111111-1"
+              <div class="col-md mb-3">
+                <label for="RUT">RUT</label>
+                <input type="text" class="form-control" id="rutprop" name="rutprop" placeholder="1111111-1"
                             oninput="checkRut(this)" required>
-                            <script>
+                <script>
                             function valida_rut() {
                                 
                                 var dato = $('#rut').val();
@@ -106,43 +56,92 @@
                                 */
                             }
                             </script>
-                            <div class="invalid-feedback"> Dígito verificador no válido. Verifica rut ingresado </div>
-                        </div>
-                      
-                    </div>
-          
-          <div class="col-md-2 mb-3 col-xl-3 col-lg-1 offset-lg-0">
-            <label for="prop">&nbsp;</label>
-            <br>
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="background-color:#536656;color:#A5CCAB;position:inherit">Buscar</button>
+                <div class="invalid-feedback">Dígito verificador no válido. Verifica rut ingresado</div>
+              </div>
+            </div>
+            <div class="col-md-2 mb-3 col-xl-3 col-lg-1 offset-lg-0">
+              <label for="prop">&nbsp;</label>
+              <br>
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="background-color:#536656;color:#A5CCAB;position:inherit">Buscar</button>
+            </div>
+            <div class="form-row">
+              <div class="col-md-4 mb-3">
+                <label for="Nombre">Nombre</label>
+                <input type="text" class="form-control" name="nombre" required>
+                <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label for="ApellidoP">Apellido Paterno</label>
+                <input type="text" class="form-control" name="apellidop" required>
+                <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label for="ApellidoM">Apellido Materno</label>
+                <input type="text" class="form-control" name="apellidom" required>
+                <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+              </div>
+            </div>
+          </div>
+          <p>Datos Asegurado<br>
+          <div class="form-row">
+            <div class="form-row">
+              <div class="col-md mb-3">
+                <label for="RUT">RUT</label>
+                <input type="text" class="form-control" id="rutaseg" name="rutaseg" placeholder="1111111-1"
+                            oninput="checkRut(this)" required>
+                <script>
+                            function valida_rut() {
+                                
+                                var dato = $('#rut').val();
+                                /*
+                                var r = confirm(
+                                    "El rut que acabas de ingresar ya se encuentra en la base de datos. ¿Deseas ver la información asociada al rut?"
+                                );
+                                if (r == true) {
+                                    $.redirect('/bamboo/listado_clientes.php', {
+                                        'dato': dato
+                                    }, 'post');
+                                } else {
+                                    location.href = "http://gestionipn.cl/bamboo/creacion_cliente.php";
+                                }
+                                */
+                            }
+                            </script>
+                <div class="invalid-feedback">Dígito verificador no válido. Verifica rut ingresado</div>
+              </div>
+            </div>
+            <div class="col-md-2 mb-3 col-xl-3 col-lg-1 offset-lg-0">
+              <label for="prop">&nbsp;</label>
+              <br>
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="background-color:#536656;color:#A5CCAB;position:inherit">Buscar</button>
+            </div>
+            <div class="form-row">
+              <div class="col-md-4 mb-3">
+                <label for="Nombre">Nombre</label>
+                <input type="text" class="form-control" name="nombreaseg" required>
+                <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label for="ApellidoP">Apellido Paterno</label>
+                <input type="text" class="form-control" name="apellidopaseg" required>
+                <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label for="ApellidoM">Apellido Materno</label>
+                <input type="text" class="form-control" name="apellidomaseg" required>
+                <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+              </div>
+            </div>
           </div>
           <div class="form-row">
-            <div class="col-md-4 mb-3">
-              <label for="Nombre">Nombre</label>
-              <input type="text" class="form-control" name="nombreaseg" required>
-              <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-            </div>
-            <div class="col-md-4 mb-3">
-              <label for="ApellidoP">Apellido Paterno</label>
-              <input type="text" class="form-control" name="apellidopaseg" required>
-              <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-            </div>
-            <div class="col-md-4 mb-3">
-              <label for="ApellidoM">Apellido Materno</label>
-              <input type="text" class="form-control" name="apellidomaseg" required>
-              <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+            <div class ="form-row">
+              <div class="col col-lg-12">
+                <label for="Nombre">Grupo</label>
+                <input type="text" class="form-control" name="grupo" required>
+                <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+              </div>
             </div>
           </div>
-        </div>
-          <div class="form-row">
-			  <div class ="form-row">
-          <div class="col col-lg-12">
-            <label for="Nombre">Grupo</label>
-            <input type="text" class="form-control" name="grupo" required>
-            <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-          </div>
-			  </div>
-			  </div>
         </div>
       </div>
     </div>
@@ -152,9 +151,9 @@
           <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo" style="color:#536656">Compañía, Vigencia y Materia</button>
         </h5>
       </div>
-      <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
         <div class="card-body">
-          <p>Compañía<br>
+			<label for="compania"><b>Compañía</b></label><br>
           <div class="form-row">
             <div class="form-inline">
               <label for="sel1">Selecionar Compañía:&nbsp;</label>
@@ -174,6 +173,20 @@
                 <option>Southbridge</option>
                 <option>Sura</option>
                 <option>Unnio</option>
+              </select>
+              <label for="sel1">&nbsp&nbsp&nbsp&nbsp;Ramo:&nbsp;</label>
+              <select class="form-control" id="selcompania">
+                <option>VEH</option>
+                <option>Hogar</option>
+                <option>A. VIAJE</option>
+                <option>RC</option>
+                <option>INC</option>
+                <option>APV</option>
+                <option>D&O</option>
+                <option>AP</option>
+                <option></option>
+                <option>Vida</option>
+                <option>Garantía</option>
               </select>
             </div>
           </div>
@@ -198,8 +211,28 @@
               <input type="text" class="form-control" name="polizaantigua" >
             </div>
           </div>
+			<div class="form-row">
+			<div class="col-md-4 mb-3">
+              <label for="poliza">Número de Poliza</label>
+              <input type="text" class="form-control" name="poliza" required>
+              <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+            </div>
+			<div class="col-md-4 mb-3">
+              <label for="poliza">Materia Asegurada</label>
+              <input type="text" class="form-control" name="materia" required>
+              <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+            </div>
+				<div class="col-md-4 mb-3">
+              <label for="poliza">Patente o Ubicación</label>
+              <input type="text" class="form-control" name="materia" required>
+              <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
+            </div>
+				
+				
+			</div>
         </div>
-        <p>Datos Asegurado<br>
+        
+        <!--
         <div class="form-row">
           <div class="col-md-4 mb-3 col-lg-3" style= "align:left">
             <label for="RUT" style= "align:left">RUT</label>
@@ -234,71 +267,12 @@
             </div>
           </div>
         </div>
+-->
       </div>
     </div>
   </div>
   <br>
   <form action="backend/clientes/crea_cliente.php" class="needs-validation" method="POST" novalidate>
-    <h5 class="form-row">&nbsp;Datos Póliza</h5>
-    <br>
-    <div class="form-row">
-      <div class="col-md-4 mb-3">
-        <label for="Nombre">Nombre</label>
-        <input type="text" class="form-control" name="nombre" required>
-        <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-      </div>
-      <div class="col-md-4 mb-3">
-        <label for="ApellidoP">Apellido Paterno</label>
-        <input type="text" class="form-control" name="apellidop" required>
-        <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-      </div>
-      <div class="col-md-4 mb-3">
-        <label for="ApellidoM">Apellido Materno</label>
-        <input type="text" class="form-control" name="apellidom" required>
-        <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-      </div>
-      <div class="col-md-4 mb-3">
-        <div class="form-row">
-          <div class="col-md-8 mb-3">
-            <label for="RUT">RUT</label>
-            <input type="text" class="form-control" name="rut" placeholder="11111111" required>
-            <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-          </div>
-          <div class="col-md-8 mb-3 col-xl-3">
-            <label for="RUT">&nbsp;</label>
-            <input type="text" class="form-control" name="dv" placeholder="K" required>
-            <div class="invalid-feedback"></div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-3">
-        <label for="validationCustomUsername">E-mail</label>
-        <div class="input-group">
-          <div class="input-group-prepend"><span class="input-group-text" id="mail">@</span></div>
-          <input type="email" class="form-control" name="correo_electronico" required>
-          <div class="invalid-feedback">Campo en blanco o sin formato mail (aaa@bbb.xxx)</div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-3">
-        <label for="validationCustomUsername">Telefono</label>
-        <div class="input-group">
-          <input type="text" class="form-control" name="telefono" placeholder="569XXXXXX" required>
-          <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-3">
-        <label for="Dirección">Dirección Particular</label>
-        <input type="text" class="form-control" name="direccionp" required>
-        <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-      </div>
-      <div class="col-md-4 mb-3">
-        <label for="validationCustomUsername">Dirección Laboral</label>
-        <div class="input-group">
-          <input type="text" class="form-control" name="direccionl" required>
-          <div class="invalid-feedback">No puedes dejar este campo en blanco</div>
-        </div>
-      </div>
-    </div>
     <button class="btn" type="submit" style="background-color: #536656; color: white">Registrar</button>
   </form>
 </div>
