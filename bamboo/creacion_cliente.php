3d4e3763bd0b51388e353fcd29173387dba5b387
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-02-29 11:08:43 -0300

    feabarcas v2.61

--- bamboo/creacion_cliente.php
+++ bamboo/creacion_cliente.php
@@ -59,6 +59,15 @@ return $resultado;
     <div class="container">
         <p> Clientes / Creación <br>
         </p>
+        <div class="alert alert-danger" role="alert" id="alertas" style="visibility: hidden">
+            <h4 class="alert-heading">Rut duplicado</h4>
+            <p>El rut que acabas de ingresar ya se encuentra en la base de datos.</p>
+            <hr>
+            <p class="mb-0">¿Deseas ver la información asociada al rut?</p>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
         <form action="/bamboo/backend/clientes/crea_cliente.php" class="needs-validation" method="POST" novalidate>
             <h5 class="form-row">&nbsp;Datos personales</h5>
             <br>
@@ -82,12 +91,18 @@ return $resultado;
                     <div class="form-row">
                         <div class="col-md-8 mb-3">
                             <label for="RUT">RUT</label>
-                            <input type="text" class="form-control" id="id_rut" name="rut" placeholder="11111111" onchange="test()" required> 
-                                <script>
-                                function test(){
+                            <input type="text" class="form-control" id="id_rut" name="rut" placeholder="11111111"
+                                onchange="test()" required>
+                            <script>
+                            function test() {
                                 var dato = $('#id_rut').val();
-                                alert(dato)}
-                                </script>
+                                alert(
+                                    'El rut que acabas de ingresar ya se encuentra en la base de datos. ¿Deseas ver la información asociada al rut?'
+                                    )
+                            }
+                            element = document.querySelector('#alertas');
+                            element.style.visibility = 'visible'
+                            </script>
                             <div class="invalid-feedback"> No puedes dejar este campo en blanco </div>
                         </div>
                         <div class="col-md-8 mb-3 col-xl-3">
@@ -129,28 +144,29 @@ return $resultado;
             <button class="btn" type="submit" style="background-color: #536656; color: white">Registrar</button>
         </form>
     </div>
-	<script>
-	(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
 
-	</script>
-	
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+
+    <script>
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
