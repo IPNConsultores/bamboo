5afd1819673d2c71b01444d92a13ba8636fc16a6
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-03-01 18:03:06 -0300

    feabarcas v2.98

--- bamboo/test.php
+++ bamboo/test.php
@@ -57,30 +57,7 @@ echo '</table>' ;
 
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Creación Clientes</title>
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
-
-
-</head>
-
-
-<body>
-    <input type="text" class="form-control" id="rut" name="rut" placeholder="1111111-1" required>
-    <button onclick="valida_rut()" class="btn" style="background-color: #536656; color: white">Registrar</button>
-
-    <script>
+<script>
     function valida_rut() {
 
         var dato = $('#rut').val();
@@ -103,7 +80,7 @@ echo '</table>' ;
             fail: function(error) {
                 alert("error" + error);
             }
-        });
+        });}
 
 /*
         
@@ -120,8 +97,48 @@ echo '</table>' ;
                     }
                 }
                 */
-    }
+                $(document).ready(function () {
+	$("#btnsend").click(function () {
+		$.ajax({
+			type: "GET",
+			url: "/bamboo/backend/clientes/clientes_duplicados.php",
+            data: {
+                "rut": rut_sin_dv
+            }
+			success: function (data) {
+				$("#retriever").text(data);
+			}
+		});
+	});
+});
+
     </script>
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Creación Clientes</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+
+
+</head>
+
+
+<body>
+    <input type="text" class="form-control" id="rut" name="rut" placeholder="1111111-1" required>
+    <button onclick="valida_rut()" id="btnsend" class="btn" style="background-color: #536656; color: white">Registrar</button>
+    <div id="retriever"></div>
+
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
