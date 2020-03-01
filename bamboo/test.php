21fecb14a2ae9d3da48a0a20469f4367e35bfba3
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-03-01 18:05:39 -0300

    feabarcas v2.99

--- bamboo/test.php
+++ bamboo/test.php
@@ -124,11 +124,7 @@ echo '</table>' ;
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Creación Clientes</title>
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
+   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 
 
 </head>
@@ -136,18 +132,9 @@ echo '</table>' ;
 
 <body>
     <input type="text" class="form-control" id="rut" name="rut" placeholder="1111111-1" required>
-    <button onclick="valida_rut()" id="btnsend" class="btn" style="background-color: #536656; color: white">Registrar</button>
+    <button  id="btnsend" class="btn" style="background-color: #536656; color: white">Registrar</button>
     <div id="retriever"></div>
 
-
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
-    </script>
-    <script src="/bamboo/js/jquery.redirect.js"></script>
-    <script src="/bamboo/js/validarRUT.js"></script>
 </body>
 
 </html>
