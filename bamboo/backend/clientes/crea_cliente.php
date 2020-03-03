5ee800c1e4e778bc54df62acdc635937d90b6966
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-03-03 18:37:37 -0300

    feabarcas v3.42

--- bamboo/backend/clientes/crea_cliente.php
+++ bamboo/backend/clientes/crea_cliente.php
@@ -15,9 +15,9 @@ mysqli_set_charset( $link, 'utf8');
 mysqli_select_db($link, 'gestio10_asesori1_bamboo');
 mysqli_query($link, 'insert into clientes(nombre_cliente, apellido_paterno, apellido_materno, rut_sin_dv, dv, direccion_personal, correo,direccion_laboral, telefono) values (\''.$nombre.'\', \''.$apellidop.'\', \''.$apellidom.'\', \''.$rut.'\', \''.$dv.'\', \''.$direccionp.'\', \''.$correo_electronico.'\', \''.$direccionl.'\', \''.$telefono.'\');');
 
-echo '<script type="text/javascript">
-redirige('.$rut.');
-</script>';
+//echo '<script type="text/javascript">
+//redirige('.$rut.');
+//</script>';
 //header("Location:http://gestionipn.cl/bamboo/index.php");
 
 function estandariza_info($data) {
@@ -42,11 +42,11 @@ window.document.onload = function(e){
     (window.tdiff[0] = Date.now()) && window.tdiff.reduce(fred) ); 
 }
 
-function redirige($dato){
+/*function redirige($dato){
   $.redirect('/bamboo/listado_clientes.php', {
   'dato': $dato
 }, 'post');
-}
+}*/
 
 </script>
 </body>
