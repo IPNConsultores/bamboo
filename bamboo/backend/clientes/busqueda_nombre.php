d29913b2176db7fdd8859fe7d63ab619b905023c
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-03-03 22:00:23 -0300

    feabarcas v3.63

--- bamboo/backend/clientes/busqueda_nombre.php
+++ bamboo/backend/clientes/busqueda_nombre.php
@@ -1,28 +1,32 @@
 <?php
 require_once "/home/gestio10/public_html/backend/config.php";
 $resultado = '';
-if (isset($_POST['rut']) && !empty($_POST['rut']))
-{ $busqueda=17029236;
+//if (isset($_POST['rut']) && !empty($_POST['rut']))
+{ 
+    $busqueda=17029236;
+    echo $busqueda;
     mysqli_set_charset($link, 'utf8');
     mysqli_select_db($link, 'gestio10_asesori1_bamboo');
-    //$sql = "SELECT id FROM clientes WHERE CONTACT(rut_sin_dv, \'-\',dv) = ?";
+        //$sql = "SELECT id FROM clientes WHERE CONTACT(rut_sin_dv, \'-\',dv) = ?";
     $sql = "SELECT id, nombre, apellidop, apellidom FROM clientes WHERE rut_sin_dv =".$busqueda.";";
     mysqli_select_db($link, 'gestio10_asesori1_bamboo');
- $resultado=mysqli_query($link,$sql);
-While($row=mysqli_fetch_object($resultado))
-{
-   //Mostramos los titulos de los articulos o lo que deseemos...
-  $rut=$row["rut"];
-   $nombre=$row["nombre"];
-   $apellidop=$row["apellidop"];
-   $apellidom=$row["apellidom"];
-   echo $apellidom;
-   echo json_encode(array(
-    "rut" =>& $rut,
-    "nombre"=>& $nombre,
-    "apellidop"=>& $apellidop,
-    "apellidom"=>& $apellidom
-));
-}   
+    $resultado=mysqli_query($link,$sql);
+    While($row=mysqli_fetch_object($resultado))
+    {
+        //Mostramos los titulos de los articulos o lo que deseemos...
+        $rut=$row["rut"];
+        $nombre=$row["nombre"];
+        $apellidop=$row["apellidop"];
+        $apellidom=$row["apellidom"];
+        echo $apellidom;
+       
+        echo json_encode(array(
+            "rut" =>& $rut,
+            "nombre"=>& $nombre,
+            "apellidop"=>& $apellidop,
+            "apellidom"=>& $apellidom
+        ));
+        
+    }
 }
 ?>
