a454659b1a2d56674d66cb90f86ae1f8c94c001b
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-03-07 18:39:46 -0300

    feabarcas v3.85

--- bamboo/test.php
+++ bamboo/test.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/css/datatables.min.css">
+        <link rel="stylesheet" href="/assets/css/datatables.min.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
@@ -45,7 +45,7 @@
     <script src="/assets/js/jquery.redirect.js"></script>
     <script src="/assets/js/bootstrap-notify.js"></script>
     <script src="/assets/js/bootstrap-notify.min.js"></script>
-    <script src="assets/js/datatables.min.js"></script>
+    <script src="/assets/js/datatables.min.js"></script>
 </body>
 
 </html>
