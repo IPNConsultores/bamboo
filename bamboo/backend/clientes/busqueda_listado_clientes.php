4d94e2e9e0d1c764e9bcbece6c9f2816b947430c
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-03-07 18:03:03 -0300

    feabarcas v3.83

--- bamboo/backend/clientes/busqueda_listado_clientes.php
+++ bamboo/backend/clientes/busqueda_listado_clientes.php
@@ -1,25 +1,24 @@
 <?php
-require_once "/home/gestio10/public_html/backend/config.php";
 $resultado ='';
-        mysqli_set_charset($link, 'utf8');
+require_once "/home/gestio10/public_html/backend/config.php";
+
+    mysqli_set_charset($link, 'utf8');
     mysqli_select_db($link, 'gestio10_asesori1_bamboo');
     //$sql = "SELECT id FROM clientes WHERE CONTACT(rut_sin_dv, \'-\',dv) = ?";
-    $sql = "SELECT CONCAT(rut_sin_dv, \'-\',dv) as rut, apellido_materno, apellido_paterno, correo, direccion_laboral, direccion_personal, id, nombre_cliente, telefono
-    FROM clientes";
-  $resultado=mysqli_query($link, $sql);
+    $sql = "SELECT CONCAT(rut_sin_dv, \'-\',dv) as rut, apellido_materno, apellido_paterno, correo, direccion_laboral, direccion_personal, id, nombre_cliente, telefono FROM clientes";
+    $resultado=mysqli_query($link, $sql);
   While($row=mysqli_fetch_object($resultado))
   {
     echo json_encode(array(
-            "resultado" => "antiguo",
-            "id" =>& $id,
-            "nombre"=>& $nombre_cliente,
-            "apellidop"=>& $apellido_paterno,
-            "apellidom"=>& $apellido_materno,
-            "correo_electronico" =>& $correo,
-            "direccionl" =>& $direccion_laboral,
-            "direccionp" =>& $direccion_personal,
-            "telefono" =>& $telefono,
-            "rut" =>& $rut
+            "id" =>& $row["id"],
+            "nombre"=>& $row["nombre_cliente"],
+            "apellidop"=>& $row["apellido_paterno"],
+            "apellidom"=>& $row["apellido_materno"],
+            "correo_electronico" =>& $row["correo"],
+            "direccionl" =>& $row["direccion_laboral"],
+            "direccionp" =>& $row["direccion_personal"],
+            "telefono" =>& $row["telefono"],
+            "rut" =>& $row["rut"]
         ));
 }
 
