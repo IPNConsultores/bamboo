4ea4a27aa3d7a9ea8463063b3db255874a310544
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-03-08 17:29:45 -0300

    feabarcas v3.93 -> datatables, buttons

--- bamboo/test.php
+++ bamboo/test.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -10,7 +9,7 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" href="/assets/css/datatables.min.css">
+    <link rel="stylesheet" href="/assets/css/datatables.min.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
@@ -27,21 +26,21 @@
         </p>
         <br>
         <table id="listado_clientes" class="display" width="100%">
-                    <tr>
-                        <thead>
-                            <th></th>
-                            <th>Rut</th>
-                            <th>Nombre</th>
-                            <th>Apellido paterno</th>
-                            <th>Apellido materno</th>
-                            <th>Teléfono</th>
-                            <th>e-mail</th>
-                            <th>Dirección Privada</th>
-                            <th>Dirección Laboral</th>
-                            <th>id</th>
-                        </thead>
-                    </tr>
-                </table>
+            <tr>
+                <thead>
+                    <th></th>
+                    <th>Rut</th>
+                    <th>Nombre</th>
+                    <th>Apellido paterno</th>
+                    <th>Apellido materno</th>
+                    <th>Teléfono</th>
+                    <th>e-mail</th>
+                    <th>Dirección Privada</th>
+                    <th>Dirección Laboral</th>
+                    <th>id</th>
+                </thead>
+            </tr>
+        </table>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
@@ -55,85 +54,125 @@
 
 </html>
 <script>
-
-    $(document).ready(function() {
-   var table = $('#listado_clientes').DataTable( {
-    "ajax": "/bamboo/backend/clientes/busqueda_listado_clientes.php",
-        "columns": [
-            {
-                "className":      'details-control',
-                "orderable":      false,
-                "data":           null,
+$(document).ready(function() {
+    var table = $('#listado_clientes').DataTable({
+        "ajax": "/bamboo/backend/clientes/busqueda_listado_clientes.php",
+        "columns": [{
+                "className": 'details-control',
+                "orderable": false,
+                "data": null,
                 "defaultContent": '<i class="fas fa-search-plus"></i>'
             },
-            { "data": "rut" },
-            { "data": "nombre" },
-            { "data": "apellidop" },
-            { "data": "apellidom" },
-            { "data": "telefono" },
-            { "data": "correo_electronico" },
-            { "data": "direccionp" },
-            { "data": "direccionl" },
-            { "data": "id" }
-            
-        ],
-        "columnDefs": [
             {
-                "targets": [ 6,7,8,9 ],
+                "data": "rut"
+            },
+            {
+                "data": "nombre"
+            },
+            {
+                "data": "apellidop"
+            },
+            {
+                "data": "apellidom"
+            },
+            {
+                "data": "telefono"
+            },
+            {
+                "data": "correo_electronico"
+            },
+            {
+                "data": "direccionp"
+            },
+            {
+                "data": "direccionl"
+            },
+            {
+                "data": "id"
+            }
+
+        ],
+        //          "search": {
+        //          "search": "abarca"
+        //          },
+        "columnDefs": [{
+                "targets": [6, 7, 8, 9],
                 "visible": false,
             },
             {
-                "targets": [ 5,6,7,8,9 ],
+                "targets": [5, 6, 7, 8, 9],
                 "searchable": false
             }
         ],
-        "order": [[ 2, "asc" ], [ 3, "asc" ]]
-    } );
-       $('#listado_clientes tbody').on('click', 'td.details-control', function () {
+        "order": [
+            [3, "asc"],
+            [4, "asc"]
+        ],
+        "oLanguage": {
+            "sSearch": "Búsqueda rápida",
+            "sLengthMenu": 'Mostrar <select>' +
+                '<option value="10">10</option>' +
+                '<option value="25">30</option>' +
+                '<option value="50">50</option>' +
+                '<option value="-1">todos</option>' +
+                '</select> registros',
+            "sInfoFiltered": "(filtrado de _MAX_ registros totales)",
+            "sLengthMenu": "Muestra _MENU_ registros por página",
+            "sZeroRecords": "No hay registros asociados",
+            "sInfo": "Mostrando página _PAGE_ de _PAGES_",
+            "sInfoEmpty": "No hay registros disponibles",
+            "oPaginate": {
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior",
+                "sLast": "Última"
+            }
+        }
+    });
+    $('#listado_clientes tbody').on('click', 'td.details-control', function() {
         var tr = $(this).closest('tr');
-        var row = table.row( tr );
- 
-        if ( row.child.isShown() ) {
+        var row = table.row(tr);
+
+        if (row.child.isShown()) {
             // This row is already open - close it
             row.child.hide();
             tr.removeClass('shown');
-        }
-        else {
+        } else {
             // Open this row
-            row.child( format(row.data()) ).show();
+            row.child(format(row.data())).show();
             tr.addClass('shown');
         }
-    } );
-} );
+    });
+});
 
-function format ( d ) {
+function format(d) {
     // `d` is the original data object for the row
-    return '<table background-color:#F6F6F6; color:#FFF; cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-        '<tr>'+
-            '<td>Nombre completo:</td>'+
-            '<td>'+d.nombre+' '+d.apellidop+' '+d.apellidom+'</td>'+
-            '</tr>'+
-                    '<tr>'+
-            '<td>Correo electrónico:</td>'+
-            '<td>'+d.correo_electronico+'</td>'+
-            '</tr>'+
-                                '<tr>'+
-            '<td>Dirección particular:</td>'+
-            '<td>'+d.direccionp+'</td>'+
-            '</tr>'+
-                                '<tr>'+
-            '<td>Dirección laboral:</td>'+
-            '<td>'+d.direccionl+'</td>'+
-            '</tr>'+
-                        '</tr>'+
+    return '<table background-color:#F6F6F6; color:#FFF; cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+        '<tr>' +
+        '<td>Nombre completo:</td>' +
+        '<td>' + d.nombre + ' ' + d.apellidop + ' ' + d.apellidom + '</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Correo electrónico:</td>' +
+        '<td>' + d.correo_electronico + '</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Dirección particular:</td>' +
+        '<td>' + d.direccionp + '</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Dirección laboral:</td>' +
+        '<td>' + d.direccionl + '</td>' +
+        '</tr>' +
+        '</tr>' +
 
-            '<tr>'+
-            '<td>Acciones</td>'+
-            '<td><button title="Busca toda la información asociada a este cliente" type="button" id="+d.id+" name="info" onclick="botones(this.id, this.name)"><i class="fas fa-search"></i></button><a> </a><button title="Modifica la información de este cliente"  type="button" id="+d.id+" name="modifica" onclick="botones(this.id, this.name)"><i class="fas fa-edit"></i></button><a> </a><button title="Elimina este cliente"  type="button" id="+d.id+" name="elimina" onclick="botones(this.id, this.name)"><i class="fas fa-trash-alt"></i></button><a> </a><button title="Asigna una tarea o comentario"  type="button" id="+d.id+" name="tarea" onclick="botones(this.id, this.name)"><i class="fas fa-clipboard-list"></i></button></td>'+
-            
-        '</tr>'+
-    '</table>';
+        '<tr>' +
+        '<td>Acciones</td>' +
+        '<td><button title="Busca toda la información asociada a este cliente" type="button" id="+d.id+" name="info" onclick="botones(this.id, this.name)"><i class="fas fa-search"></i></button><a> </a><button title="Modifica la información de este cliente"  type="button" id="+d.id+" name="modifica" onclick="botones(this.id, this.name)"><i class="fas fa-edit"></i></button><a> </a><button title="Elimina este cliente"  type="button" id="+d.id+" name="elimina" onclick="botones(this.id, this.name)"><i class="fas fa-trash-alt"></i></button><a> </a><button title="Asigna una tarea o comentario"  type="button" id="+d.id+" name="tarea" onclick="botones(this.id, this.name)"><i class="fas fa-clipboard-list"></i></button></td>' +
+
+        '</tr>' +
+        '</table>';
 }
+
 function botones(id, accion) {
     console.log("ID:" + id + " => acción:" + accion);
     switch (accion) {
@@ -202,5 +241,4 @@ function botones(id, accion) {
         }
     }
 }
-
 </script>
