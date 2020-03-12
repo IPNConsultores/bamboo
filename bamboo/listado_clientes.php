6e01f14849ca1f655674f82e90c1187426caa596
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-03-11 22:53:44 -0300

    feabarcas v4.06

--- bamboo/listado_clientes.php
+++ bamboo/listado_clientes.php
@@ -198,8 +198,8 @@ console.log(fecha+' <--'+dd)
                     filename: 'Listado clientes al: '+fecha,
                     exportOptions: {
                     columns: [1, 2,3,4,5,6,7,8]}
-                }
                 },
+                
        {
                     
                     orientation: 'landscape',
