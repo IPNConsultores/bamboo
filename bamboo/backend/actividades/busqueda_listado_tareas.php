b559f7f412ce42114e9628545a73fea9490c2ad6
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-03-15 22:26:54 -0300

    feabarcas v4.10

--- bamboo/backend/actividades/busqueda_listado_tareas.php
+++ bamboo/backend/actividades/busqueda_listado_tareas.php
@@ -6,7 +6,7 @@ require_once "/home/gestio10/public_html/backend/config.php";
     mysqli_set_charset($link, 'utf8');
     mysqli_select_db($link, 'gestio10_asesori1_bamboo');
     //$sql = "SELECT id FROM clientes WHERE CONTACT(rut_sin_dv, \'-\',dv) = ?";
-    $sql = "select prioridad, estado, tarea, fecha_vencimiento, id_poliza as poliza, CONCAT(nombre_cliente, ' ', apellido_paterno, ' ', apellido_materno) as nom_cliente, CONCAT(rut_sin_dv, '-',dv) as rut_cliente, telefono, correo,a.id as id_tarea from tareas as a left join clientes as b on a.id_cliente=b.rut_sin_dv and b.rut_sin_dv is not null";
+    $sql = "select prioridad, estado, tarea, fecha_vencimiento, id_poliza as poliza, CONCAT(nombre_cliente, ' ', apellido_paterno, ' ', apellido_materno) as nom_cliente, CONCAT(rut_sin_dv, '-',dv) as rut_cliente, telefono, correo, a.id as id_tarea from tareas as a left join clientes as b on a.id_cliente=b.rut_sin_dv and b.rut_sin_dv is not null";
     $resultado=mysqli_query($link, $sql);
     $codigo='{
       "data": [';
@@ -23,7 +23,8 @@ require_once "/home/gestio10/public_html/backend/config.php";
         "nom_cliente" =>& $row->nom_cliente,
         "rut_cliente" =>& $row->rut_cliente,
         "telefono" =>& $row->telefono,
-        "correo" =>& $row->correo));
+        "correo" =>& $row->correo,
+        "id_tarea" =>& $row->id_tarea));
     } else {
     $codigo.= ', '.json_encode(array(
       "prioridad" =>& $row->prioridad,
@@ -34,7 +35,8 @@ require_once "/home/gestio10/public_html/backend/config.php";
       "nom_cliente" =>& $row->nom_cliente,
       "rut_cliente" =>& $row->rut_cliente,
       "telefono" =>& $row->telefono,
-      "correo" =>& $row->correo
+      "correo" =>& $row->correo,
+      "id_tarea" =>& $row->id_tarea
     ));}
   }
   $codigo.=']}';
--- bamboo/index.php
+++ bamboo/index.php
@@ -12,26 +12,6 @@ function estandariza_info($data) {
   }
 require_once "/home/gestio10/public_html/backend/config.php";
 $num=0;
-
-    mysqli_set_charset( $link, 'utf8');
-    mysqli_select_db($link, 'gestio10_asesori1_bamboo');
-    $resultado=mysqli_query($link, 'SELECT * FROM tareas where estado =\'activo\'order by fecha_vencimiento desc, prioridad asc;');
-    While($row=mysqli_fetch_object($resultado))
-        {
-        //Mostramos los titulos de los articulos o lo que deseemos...
-            $id=$row->id;
-            $id_cliente=$row->id_cliente;
-            $id_poliza=$row->id_poliza;
-            $fecha_ingreso=$row->fecha_ingreso;
-            $fecha_vencimiento=$row->fecha_vencimiento;
-            $tarea=$row->tarea;
-            $estado=$row->estado;
-            $prioridad=$row->prioridad;
-            $num=$num+1;
-            $lista=$lista.'<tr><td>'.$num.'</td><td>'.$prioridad.'</td><td>'.$estado.'</td><td>'.$tarea.'</td><td>'.$fecha_vencimiento.'</td><td>'.$id_poliza.'</td><td>'.$fecha_vencimiento.'</td><td><a class="button" name="boton-modificar" href="#">Marcar como completada</a><a> </a><a class="button" name="boton-elimina-cliente" href="#">Eliminar</a></td><tr>'. "<br>";
-               
-        }
-    mysqli_close($link);
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -44,6 +24,11 @@ $num=0;
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/datatables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.dataTables.min.css" />
+
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
@@ -60,28 +45,34 @@ $num=0;
         </p>
         <br>
     </div>
+
     <div class="accordion" id="accordionExample">
         <div class="card">
             <div class="card-header" id="headingOne" style="background-color:whitesmoke">
                 <h5 class="mb-0">
                     <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
-                        aria-expanded="true" aria-controls="collapseOne" style="color:#536656">Tareas y compromisos</button>
+                        aria-expanded="true" aria-controls="collapseOne" style="color:#536656">Tareas y
+                        compromisos</button>
                 </h5>
             </div>
             <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
                 <div class="card-body">
-                <table class="display" width="100%" id="listado_tareas">
-                    <tr>
-                        <th>#</th>
-                        <th>Prioridad</th>
-                        <th>Estado</th>
-                        <th>Tarea</th>
-                        <th>Fecha vencimiento</th>
-                        <th>póliza asociada</th>
-                        <th>Cliente asociado</th>
-                        <th>Acción</th>
-                    </tr>
-                </table>
+                    <table class="display" width="100%" id="listado_tareas">
+                        <tr>
+                            <th></th>
+                            <th>Prioridad</th>
+                            <th>Estado</th>
+                            <th>Tarea</th>
+                            <th>Fecha vencimiento</th>
+                            <th>póliza asociada</th>
+                            <th>Cliente asociado</th>
+                            <th>Rut Cliente</th>
+                            <th>Teléfono</th>
+                            <th>e-Mail</th>
+                            <th>id tarea</th>
+                        </tr>
+                    </table>
+                    <div id="botones"></div>
                 </div>
             </div>
         </div>
@@ -95,28 +86,42 @@ $num=0;
             </div>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                 <div class="card-body">
-                <table class="table" id="listado_tareas">
-                    <tr>
-                        <th>#</th>
-                        <th>Prioridad</th>
-                        <th>Estado</th>
-                        <th>Tarea</th>
-                        <th>Fecha vencimiento</th>
-                        <th>póliza asociada</th>
-                        <th>Cliente asociado</th>
-                        <th>Acción</th>
-                    </tr>
-                </table>
+                    <table class="table" id="listado_tareas1">
+                        <tr>
+                            <th></th>
+                            <th>Prioridad</th>
+                            <th>Estado</th>
+                            <th>Tarea</th>
+                            <th>Fecha vencimiento</th>
+                            <th>póliza asociada</th>
+                            <th>Cliente asociado</th>
+                            <th>Rut Cliente</th>
+                            <th>Teléfono</th>
+                            <th>e-Mail</th>
+                            <th>id tarea</th>
+                        </tr>
+                    </table>
+                    <div id="botones"></div>
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
         integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
     </script>
+    <script src="/assets/js/jquery.redirect.js"></script>
+    <script src="/assets/js/bootstrap-notify.js"></script>
+    <script src="/assets/js/bootstrap-notify.min.js"></script>
+    <script src="/assets/js/datatables.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
 </body>
 
 </html>
@@ -167,12 +172,11 @@ $(document).ready(function() {
         //          "search": "abarca"
         //          },
         "columnDefs": [{
-                "targets": [7, 8, 9],
-                "visible": false,
-            }
-        ],
+            "targets": [7, 8, 9, 10],
+            "visible": false,
+        }],
         "order": [
-            
+
             [2, "desc"],
             [4, "asc"],
             [1, "asc"]
@@ -211,7 +215,6 @@ $(document).ready(function() {
             tr.addClass('shown');
         }
     });
-    $('#listado_tareas').dataTable().fnFilter(document.getElementById("var1").value);
     var dd = new Date();
     var fecha = '' + dd.getFullYear() + '-' + (("0" + (dd.getMonth() + 1)).slice(-2)) + '-' + (("0" + (dd
         .getDate() + 1)).slice(-2)) + ' (' + dd.getHours() + dd.getMinutes() + dd.getSeconds() + ')';
@@ -273,5 +276,4 @@ function format(d) {
         '</tr>' +
         '</table>';
 }
-
 </script>
