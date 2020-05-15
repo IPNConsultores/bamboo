c07b5f93b36e143bd37ccc564cf842d1eb18623e
IPNConsultores <transbank01>
2020-05-14 20:09:38 -0400

    template poliza V2.22

--- bamboo/template_poliza.php
+++ bamboo/template_poliza.php
@@ -78,6 +78,15 @@ $resultado_template=mysqli_query($link, 'SELECT template FROM template_correos w
 <?php
 $subject = urlencode('Envío de documentación');
 $body = urlencode($template);
+						$body = str_replace( '_[SALTO_LINEA]_', '%0A', $body );
+                        $body = str_replace( '_[NEG_ini]_', '**', $body );
+                        $body = str_replace( '_[NEG_fin]_', '**', $body );
+                        $body = str_replace( '_[SUB_ini]_', '*', $body );
+                        $body = str_replace( '_[SUB_fin]_', '*',$body );
+                        $body = str_replace( '_[CUR_ini]_', '***', $body );
+                        $body = str_replace( '_[CUR_fin]_', '***', $body );
+                        $body = str_replace( '_[LINEA]_', '<hr>', $body );
+
 $url = htmlspecialchars("https://mail.google.com/mail/?view=cm&fs=1&su=$subject&body=$body");
 ?>
 -->
