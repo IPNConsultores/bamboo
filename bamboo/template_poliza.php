60e4e25273660c81f0f673eaecd6534c2cb021a9
IPNConsultores <transbank01>
2020-05-14 22:29:07 -0400

    template poliza V 3.17

--- bamboo/template_poliza.php
+++ bamboo/template_poliza.php
@@ -79,6 +79,8 @@ $body =str_replace( '<br>', '%0A', $body );
 $body =str_replace( '<b>', '**', $body );
 $body =str_replace( '</b>', '**', $body );
 $body =str_replace( '<hr>', '%0A%0A', $body );
+$body =str_replace( '<u>', '', $body );
+$body =str_replace( '</u>', '', $body );
 $body =urlencode($body);
 
 $url = htmlspecialchars( "https://mail.google.com/mail/?view=cm&fs=1&su=$subject&body=$body");
