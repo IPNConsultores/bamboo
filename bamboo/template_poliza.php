20cf3d43978d4e8eee1a3cd1fccfe484750b1aba
IPNConsultores <transbank01>
2020-05-17 12:32:08 -0400

    poliza_template V4.22

--- bamboo/template_poliza.php
+++ bamboo/template_poliza.php
@@ -108,7 +108,7 @@ $body =urlencode($body);
 mysqli_set_charset( $link, 'utf8' );
   mysqli_select_db( $link, 'gestio10_asesori1_bamboo' );
   //correo_Cliente
-  $resultado_correo_cliente = mysqli_query( $link, "SELECT a.correo, a.rut_sin_dv, a.id , count(b.correo) cuenta_contacto FROM clientes a LEFT JOIN clientes_contactos b on a.id = b.id_cliente where a.rut_sin_dv = $rut_proponente or a.rut_sin_dv = $rut_proponente group by a.correo, a.rut_sin_dv, a.id order by cuenta_contacto desc;"  );
+  $resultado_correo_cliente = mysqli_query( $link, 'SELECT a.correo, a.rut_sin_dv, a.id , count(b.correo) cuenta_contacto FROM clientes a LEFT JOIN clientes_contactos b on a.id = b.id_cliente where a.rut_sin_dv ='.$rut_proponente.' or a.rut_sin_dv ='.$rut_proponente.' group by a.correo, a.rut_sin_dv, a.id order by cuenta_contacto desc;"  ');
   
   $destinatario ='';
   
