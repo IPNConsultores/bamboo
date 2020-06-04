e05f00fa40ca8e1b6e572e507f0bf24c6bb45190
IPNConsultores <transbank01>
2020-06-03 21:48:03 -0400

    template

--- bamboo/template_poliza.php
+++ bamboo/template_poliza.php
@@ -68,80 +68,79 @@ if ( $_SERVER[ "REQUEST_METHOD" ] == "POST" ) {
     $template = str_replace( '_[LINEA]_', '<hr>', $template );
 
 
-    
   }
 }
 
 
 $body = $template;
 
-$body =str_replace( '_[SU_ini]_', '_[SU]_', $body);
-$body =str_replace( '_[SU_fin]_', '_[SU]_', $body);
+$body = str_replace( '_[SU_ini]_', '_[SU]_', $body );
+$body = str_replace( '_[SU_fin]_', '_[SU]_', $body );
 
-$array = explode('_[SU]_', $body );
-$subject = $array[1];
-$body = $array[2];
+$array = explode( '_[SU]_', $body );
+$subject = $array[ 1 ];
+$body = $array[ 2 ];
 
-$subject =str_replace( 'ASUNTO:', '', $subject );
-$subject =str_replace( '<br>', '%0A', $subject );
-$subject =str_replace( '<b>', '', $subject );
-$subject =str_replace( '</b>', '', $subject );
-$subject =str_replace( '<hr>', '%0A%0A', $subject );
-$subject =str_replace( '<u>', '', $subject );
-$subject =str_replace( '</u>', '', $subject );
-$subject =str_replace( '•', '• ', $subject );
-$subject =urlencode($subject);
+$subject = str_replace( 'ASUNTO:', '', $subject );
+$subject = str_replace( '<br>', '%0A', $subject );
+$subject = str_replace( '<b>', '', $subject );
+$subject = str_replace( '</b>', '', $subject );
+$subject = str_replace( '<hr>', '%0A%0A', $subject );
+$subject = str_replace( '<u>', '', $subject );
+$subject = str_replace( '</u>', '', $subject );
+$subject = str_replace( '•', '• ', $subject );
+$subject = urlencode( $subject );
 
-$body =str_replace( '<br>', '%0A', $body );
-$body =str_replace( '<b>', '', $body );
-$body =str_replace( '</b>', '', $body );
-$body =str_replace( '<hr>', '%0A%0A', $body );
-$body =str_replace( '%0A%0AEstimado(a)', 'Estimado(a)', $body );
-$body =str_replace( '<u>', '', $body );
-$body =str_replace( '</u>', '', $body );
-$body =str_replace( '•', '• ', $body );
-$body =urlencode($body);
+$body = str_replace( '<br>', '%0A', $body );
+$body = str_replace( '<b>', '', $body );
+$body = str_replace( '</b>', '', $body );
+$body = str_replace( '<hr>', '%0A%0A', $body );
+$body = str_replace( '%0A%0AEstimado(a)', 'Estimado(a)', $body );
+$body = str_replace( '<u>', '', $body );
+$body = str_replace( '</u>', '', $body );
+$body = str_replace( '•', '• ', $body );
+$body = urlencode( $body );
 
 
 mysqli_set_charset( $link, 'utf8' );
-  mysqli_select_db( $link, 'gestio10_asesori1_bamboo' );
-  //correo_Cliente
-  $resultado_correo_cliente = mysqli_query( $link, 'SELECT a.correo, a.rut_sin_dv, a.id , count(b.correo) cuenta_contacto FROM clientes a LEFT JOIN clientes_contactos b on a.id = b.id_cliente where a.rut_sin_dv ='.$rut_proponente.' or a.rut_sin_dv ='.$rut_asegurado.' group by a.correo, a.rut_sin_dv, a.id order by cuenta_contacto desc;');
-  
-  $destinatario ='';
-  
-  While( $row = mysqli_fetch_object( $resultado_correo_cliente ) ) {
-      
-      $correo = $row->correo;
-      $destinatario = $destinatario.";".$correo;
-      $id = $row->id;
-      $cuenta_contacto = $row->cuenta_contacto;
-      
-      
-       if($cuenta_contacto !== 0){
-          //correo_contacto
-          $resultado_correo_contacto = mysqli_query ($link, "select correo ,id_cliente as id, null as cuenta_contacto from clientes_contactos where id_cliente =".$id.";");
-          
-          While ($row2 = mysqli_fetch_object($resultado_correo_contacto)){
-              
-              $correo_contact = $row2->correo;
-              $id_cliente = $row2->id;
-              $cuenta_contacto_contact = $row2->cuenta_contacto;
-              $destinatario = $destinatario.";".$correo_contact;
-              
-                
-          }
-         
-      
-      }
-   
-      
-     
+mysqli_select_db( $link, 'gestio10_asesori1_bamboo' );
+//correo_Cliente
+$resultado_correo_cliente = mysqli_query( $link, 'SELECT a.correo, a.rut_sin_dv, a.id , count(b.correo) cuenta_contacto FROM clientes a LEFT JOIN clientes_contactos b on a.id = b.id_cliente where a.rut_sin_dv =' . $rut_proponente . ' or a.rut_sin_dv =' . $rut_asegurado . ' group by a.correo, a.rut_sin_dv, a.id order by cuenta_contacto desc;' );
+
+$destinatario = '';
+
+While( $row = mysqli_fetch_object( $resultado_correo_cliente ) ) {
+
+  $correo = $row->correo;
+  $destinatario = $destinatario . ";" . $correo;
+  $id = $row->id;
+  $cuenta_contacto = $row->cuenta_contacto;
+
+
+  if ( $cuenta_contacto !== 0 ) {
+    //correo_contacto
+    $resultado_correo_contacto = mysqli_query( $link, "select correo ,id_cliente as id, null as cuenta_contacto from clientes_contactos where id_cliente =" . $id . ";" );
+
+    While( $row2 = mysqli_fetch_object( $resultado_correo_contacto ) ) {
+
+      $correo_contact = $row2->correo;
+      $id_cliente = $row2->id;
+      $cuenta_contacto_contact = $row2->cuenta_contacto;
+      $destinatario = $destinatario . ";" . $correo_contact;
+
+
     }
 
-mysqli_close( $link );
 
-$url = htmlspecialchars( "https://mail.google.com/mail/?view=cm&fs=1&to=$destinatario&su=$subject&body=$body");
+  }
+
+
+}
+
+mysqli_close( $link );
+$template = str_replace( '_[SU_ini]_', '', $template );
+$template = str_replace( '_[SU_fin]_', '', $template );
+$url = htmlspecialchars( "https://mail.google.com/mail/?view=cm&fs=1&to=$destinatario&su=$subject&body=$body" );
 
 ?>
 
@@ -177,14 +176,13 @@ $url = htmlspecialchars( "https://mail.google.com/mail/?view=cm&fs=1&to=$destina
 </div>
 <br>
 <div name='correo'>
-  <div class=col>
-    <h6>Resultado</h6>
-    <div id="template_correo" class="form-control bg-light text-dark" rows="10"
+<div class=col>
+  <h6>Resultado</h6>
+  <div id="template_correo" class="form-control bg-light text-dark" rows="10"
                 style="height: 400px; border-style: solid;overflow-y: scroll"><?php echo $template; ?></div>
-    <br>
-    <a class="btn" type="btn"
-                        style="background-color: #536656; color: white; height: 45; align-self: center;" href="<?php echo urldecode($url); ?>" target="_blank">Enviar mail</a>
   <br>
+  <a class="btn" type="btn"
+                        style="background-color: #536656; color: white; height: 45; align-self: center;" href="<?php echo urldecode($url); ?>" target="_blank">Enviar mail</a><br>
 </div>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
