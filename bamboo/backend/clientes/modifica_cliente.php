e17c83e60d3371af5d849e01b0856a9dbbcefc0c
IPNConsultores <transbank01>
2020-06-05 16:14:49 -0400

    modifica_cliente V05062020_1.0

--- bamboo/backend/clientes/modifica_cliente.php
+++ bamboo/backend/clientes/modifica_cliente.php
@@ -12,18 +12,24 @@ $direccionp=estandariza_info($_POST["direccionp"]);
 $direccionl=estandariza_info($_POST["direccionl"]);
 $correo=estandariza_info($_POST["correo_electronico"]);
 mysqli_set_charset( $link, 'utf8');
-mysqli_query($link, 'UPDATE clientes SET nombre_cliente=\''.$nombre.'\' ,apellido_paterno=\''.$apellidop.'\' ,apellido_materno=\''.$apellidom.'\' ,rut_sin_dv=\''.$rut.'\' ,dv=\''.$dv.'\' ,telefono=\''.$telefono.'\' ,direccion_personal=\''.$direccionp.'\' ,direccion_laboral=\''.$direccionl.'\' ,correo=\''.$correo.'\'  WHERE id='.$id.';');
+$query = 'UPDATE clientes SET nombre_cliente=\''.$nombre.'\' ,apellido_paterno=\''.$apellidop.'\' ,apellido_materno=\''.$apellidom.'\' ,rut_sin_dv=\''.$rut.'\' ,dv=\''.$dv.'\' ,telefono=\''.$telefono.'\' ,direccion_personal=\''.$direccionp.'\' ,direccion_laboral=\''.$direccionl.'\' ,correo=\''.$correo.'\'  WHERE id='.$id.';';
+mysqli_query($link,$query);
 
-mysqli_query($link, 'DELETE clientes_contactos  WHERE id_cliente='.$id.';');
+$borrar=  'DELETE clientes_contactos  WHERE id_cliente='.$id.';';
+mysqli_query($link,$borrar);
 
 foreach (array_keys($_POST['nombrecontact']) as $key) {
     $nombrecontact = $_POST['nombrecontact'][$key];
     $telefonocontact = $_POST['telefonocontact'][$key];
     $emailcontact = $_POST['emailcontact'][$key];
-		
-    mysqli_query($link, "INSERT INTO clientes_contactos (id_cliente, nombre, telefono, correo) select id , '".$nombrecontact."', '".$telefonocontact."', '".$emailcontact."' from clientes where rut_sin_dv='".$rut."';");
+	
+	$agregar_contacto = "INSERT INTO clientes_contactos (id_cliente, nombre, telefono, correo) select id , '".$nombrecontact."', '".$telefonocontact."', '".$emailcontact."' from clientes where rut_sin_dv='".$rut."';";
+    mysqli_query($link, $agregar_contacto);
   }
 
+ECHO $borrar;
+ECHO $agregar_contacto;
+ECHO $query;
 
 function estandariza_info($data) {
     $data = trim($data);
