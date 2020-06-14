fcf5d21cad834778ca0907f57c8ce7600f8d7340
IPNConsultores <transbank01>
2020-06-14 19:22:41 -0400

    prueba crea_poliza

--- bamboo/backend/polizas/crea_poliza.php
+++ bamboo/backend/polizas/crea_poliza.php
@@ -52,7 +52,7 @@ mysqli_set_charset( $link, 'utf8');
 mysqli_select_db($link, 'gestio10_asesori1_bamboo');
 $query='INSERT INTO polizas (numero_boleta, comision_negativa, boleta_negativa, depositado_fecha,  estado, tipo_poliza, moneda_valor_cuota,  rut_proponente,  dv_proponente,  rut_asegurado,  dv_asegurado,  compania,  ramo,  vigencia_inicial,  vigencia_final,  numero_poliza,  cobertura,  materia_asegurada,  patente_ubicacion, moneda_poliza,  deducible,  prima_afecta,  prima_exenta,  prima_neta,  prima_bruta_anual,  monto_asegurado,  numero_propuesta,  fecha_envio_propuesta,  moneda_comision,  comision,  porcentaje_comision,  comision_bruta,  comision_neta,  forma_pago, nro_cuotas,  valor_cuota,  fecha_primera_cuota,  vendedor, nombre_vendedor, poliza_renovada,endoso,informacion_adicional) VALUES ( "'.$boleta.'","'.$comisionneg.'", "'.$boletaneg.'", "'.$fechadeposito.'", "Activo", "Nuevo","'.$moenda_cuota.'","'.$rut_prop.'","'.$dv_prop.'","'.$rut_aseg.'","'.$dv_aseg.'","'.$selcompania.'","'.$ramo.'","'.$fechainicio.'","'.$fechavenc.'","'.$nro_poliza.'","'.$cobertura.'","'.$materia.'","'.$detalle_materia.'","'.$moneda_poliza.'","'.$deducible.'","'.$prima_afecta.'","'.$prima_exenta.'","'.$prima_neta.'","'.$prima_bruta.'","'.$monto_aseg.'","'.$nro_propuesta.'","'.$fechaprop.'","'.$moneda_comision.'","'.$comision.'","'.$porcentaje_comsion.'","'.$comisionbruta.'","'.$comisionneta.'","'.$modo_pago.'","'.$cuotas.'","'.$valorcuota.'","'.$fechaprimer.'","'.$con_vendedor.'","'.$nombre_vendedor.'","'.$poliza_renovada.'","'.$endoso.'","'.$comentario.'");';
 mysqli_query($link, $query);
-//ECHO $query;
+ECHO $query;
 
 
 function estandariza_info($data) {
@@ -77,10 +77,10 @@ function cambia_puntos_por_coma($data){
 <body>
 <script >
 
-var nro_poliza= <?php echo $nro_poliza; ?>;
-  $.redirect('/bamboo/listado_polizas.php', {
-  'busqueda': nro_poliza
-}, 'post');
+//var nro_poliza= <?php echo $nro_poliza; ?>;
+//  $.redirect('/bamboo/listado_polizas.php', {
+//  'busqueda': nro_poliza
+//}, 'post');
 
 </script>
 </body>
