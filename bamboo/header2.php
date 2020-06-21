3b00496707b7a448b3d401fc964f60dc60701c63
IPNConsultores <transbank01>
2020-06-21 19:27:31 -0400

    header

--- bamboo/header2.php
+++ bamboo/header2.php
@@ -21,7 +21,8 @@ function valida_rut(){
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bamboo Seguros</title>
+    <title>Gestión IPN - Bamboo Seguros</title>
+	<link rel="icon" href="/bamboo/bamboo.png">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
