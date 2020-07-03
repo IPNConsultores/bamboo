1f8b87c3f54d1d9ce6fb8e34fdc4293dc723c184
IPNConsultores <feabarcas@ipnconsultores.cl>
2020-07-03 18:27:43 -0400

    feabarcas v6.20

--- bamboo/creacion_poliza.php
+++ bamboo/creacion_poliza.php
@@ -1434,13 +1434,9 @@ var moneda = document.getElementById("moneda_poliza").value;
             document.getElementById("materia").value = '<?php echo $materia; ?>';
             document.getElementById("detalle_materia").value = '<?php echo $detalle_materia; ?>';
             document.getElementById("deducible").value = '<?php echo $deducible; ?>';
-            
             document.getElementById("comision").value = '<?php echo $comision; ?>';
             document.getElementById("porcentaje_comsion").value = '<?php echo $porcentaje_comsion; ?>';
-            document.getElementById("comisionbruta").value = '<?php echo $comisionbruta; ?>';
-            document.getElementById("comisionneta").value = '<?php echo $comisionneta; ?>';
             document.getElementById("moneda_poliza").value = '<?php echo $moneda_poliza; ?>';
-             
             document.getElementById("prima_afecta").value = '<?php echo $prima_afecta; ?>';
             document.getElementById("prima_exenta").value = '<?php echo $prima_exenta; ?>';
             document.getElementById("prima_neta").value = '<?php echo $prima_neta; ?>';
