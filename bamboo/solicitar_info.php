b8c05df64f3029bc2eaea1ffe73e9d817652d5a7
IPNConsultores <transbank01>
2020-07-03 18:16:48 -0400

    solicitar_info

--- bamboo/solicitar_info.php
+++ bamboo/solicitar_info.php
@@ -17,17 +17,21 @@ require_once "/home/gestio10/public_html/backend/config.php";
   mysqli_select_db( $link, 'gestio10_asesori1_bamboo' );
   //poliza
   
-  $instancia = "solicitar_info";
+  
 
   
   if ( $_POST[ "tipo" ] == "buscar"){
       
        $ramo = $_POST["ramo"];
+        
+       $instancia = $_POST["instancia"];
+       
+    
   }
   
   else{
-       $ramo = "vehiculo";
-      
+       $ramo = "null";
+      $instancia = "null";
   }
   
   $ramo_poliza =$ramo;
@@ -53,7 +57,10 @@ require_once "/home/gestio10/public_html/backend/config.php";
     $template = str_replace( '_[CUR_fin]_', '</em>', $template );
     $template = str_replace( '_[LINEA]_', '<hr>', $template );
 
-
+if($template == ''){
+    
+    $template = "No se ha cargado un template para esta Instancia y/o Ramo, favor seleccione otra opción";
+}
 
 $body = $template;
 
@@ -116,42 +123,93 @@ $url = htmlspecialchars( "https://mail.google.com/mail/?view=cm&fs=1&to=$destina
 </div>
 <div class="container">
   <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST" name='enviar_template'>
-      <div id="auxiliar"  >
-      <input name="tipo" id="tipo" style="display: none;">
-    
+      <div id="auxiliar" style="display:none" >
+      <input name="instancia" id="instancia" >
+      <input name="ramo" id="ramo">
+      <input name="tipo" id="tipo">
       
     </div>
-<div class="row">
-  <div class="col-4">
+    
+<h4>Envío de Correos</h4> <br>
+<div class="form-row" style="display:flex; align-items: flex-end;">
+  <div class="col-6">
+      
     <label><b>Ramo</b></label>
-    <select class="form-control" name="ramo" id="ramo" onchange ="copia_ramo()">
-          <option value="vehiculo" <?php if ($producto == "vehiculo") echo "selected" ?> >Vehiculo</option>
-          <option value="hogar_persona" <?php if ($producto == "hogar_persona") echo "selected" ?> >Hogar persona</option>
-          <option value="hogar_pyme" <?php if ($producto == "hogar_pyme") echo "selected" ?> >Hogar PyME</option>
-          <option value="viaje" <?php if ($producto == "viaje") echo "selected" ?> >A. VIAJE</option>
-          <option value="rc_do" <?php if ($producto == "rc_do") echo "selected" ?> >RC - D&O</option>
-          <option value="inc" <?php if ($producto == "inc") echo "selected" ?> >INC</option>
-          <option value="apv" <?php if ($producto == "apv") echo "selected" ?> >APV</option>
-          <option value="ap" <?php if ($producto == "ap") echo "selected" ?> >AP</option>
-          <option value="vida" <?php if ($producto == "vida") echo "selected" ?> >Vida</option>
-          <option value="garantia" <?php if ($producto == "garantia") echo "selected" ?> >Garantía</option>
-          <option value="otro" <?php if ($producto == "otro") echo "selected" ?> >Otro</option>
-        </select>
-  </div>
-  <div class="col" style="align-self:flex-end">
-   <button class="btn" type="submit" name="buscar"
-                        style="background-color: #536656; color: white; height: 45; align-self: center" onclick="envio_data(this.name)">Buscar
-    template</button>
-  </div>
- 
-</div>
+    <select class="form-control" name="ramo2" id="ramo2" onchange ="copia_ramo()">
+        <option value="null">Selecciona un ramo</option>
+        <option value="accidentes_personales_ap"<?php if ($producto =="accidentes_personales_ap") echo "selected" ?> >ACCIDENTES PERSONALES - Accidentes Personales</option>
+        <option value="accidentes_personales_protección"<?php if ($producto =="accidentes_personales_protección") echo "selected" ?> >ACCIDENTES PERSONALES - Protección Financiera </option>
+        <option value="inc_condominio"<?php if ($producto =="inc_condominio") echo "selected" ?> >INCENDIO  - Condominio </option>
+        <option value="inc_hogar"<?php if ($producto =="inc_hogar") echo "selected" ?> >INCENDIO  - Hogar</option>
+        <option value="inc_miscelaneo"<?php if ($producto =="inc_miscelaneo") echo "selected" ?> >INCENDIO  - Misceláneos</option>
+        <option value="inc_perjuicio"<?php if ($producto =="inc_perjuicio") echo "selected" ?> >INCENDIO  - Perjuicio por Paralización</option>
+        <option value="inc_pyme"<?php if ($producto =="inc_pyme") echo "selected" ?> >INCENDIO  - Pyme</option>
+        <option value="inc_trbf"<?php if ($producto =="inc_trbf") echo "selected" ?> >INCENDIO  - TRBF (Todo Riesgo Bienes Físicos)</option>
+        <option value="rc-do"<?php if ($producto =="rc-do") echo "selected" ?> >RESPONSABILIDAD CIVIL  - D&O Condominio </option>
+        <option value="rc-rc"<?php if ($producto =="rc-rc") echo "selected" ?> >RESPONSABILIDAD CIVIL  - RC General</option>
+        <option value="veh_comerciales"<?php if ($producto =="veh_comerciales") echo "selected" ?> >VEHÍCULOS  - Vehículos Comerciales Livianos</option>
+        <option value="veh_particular"<?php if ($producto =="veh_particular") echo "selected" ?> >VEHÍCULOS  - Vehículos Particulares</option>
+        <option value="veh_pesados"<?php if ($producto =="veh_pesados") echo "selected" ?> >VEHÍCULOS  - Vehículos Pesados </option>
+        <option value="viaje"<?php if ($producto =="viaje") echo "selected" ?> >ASISTENCIA EN VIAJE</option>
+        <option value="null">--------------------------------------------------------------</option>
+        <option value="averia_maquina"<?php if ($producto =="averia_maquina") echo "selected" ?> >AVERÍA DE MAQUINARIA </option>
+        <option value="casco_aeroeo"<?php if ($producto =="casco_aeroeo") echo "selected" ?> >CASCO - Aéreo</option>
+        <option value="casco_marítimo"<?php if ($producto =="casco_marítimo") echo "selected" ?> >CASCO - Marítimo </option>
+        <option value="garantia"<?php if ($producto =="garantia") echo "selected" ?> >GARANTÍA</option>
+        <option value="ing_contratístas"<?php if ($producto =="ing_contratístas") echo "selected" ?> >INGENIERÍA - Equipo Contratistas</option>
+        <option value="ing_equipo_movil"<?php if ($producto =="ing_equipo_movil") echo "selected" ?> >INGENIERÍA - Equipo Móvil Agrícola</option>
+        <option value="ing_electronicos"<?php if ($producto =="ing_electronicos") echo "selected" ?> >INGENIERÍA - Equipos Electrónicos</option>
+        <option value="ing-trc"<?php if ($producto =="ing-trc") echo "selected" ?> >INGENIERÍA - TRC (Todo Riesgo Construcción)</option>
+        <option value="remesa"<?php if ($producto =="remesa") echo "selected" ?> >REMESA DE VALORES</option>
+        <option value="robo"<?php if ($producto =="robo") echo "selected" ?> >ROBO CON FUERZA EN LAS COSAS Y VIOLENCIA EN LAS PERSONAS</option>
+        <option value="cristales"<?php if ($producto =="cristales") echo "selected" ?> >ROTURA DE CRISTALES</option>
+        <option value="seguro_arriendo"<?php if ($producto =="seguro_arriendo") echo "selected" ?> >SEGURO ARRIENDO </option>
+        <option value="seguro_credito"<?php if ($producto =="seguro_credito") echo "selected" ?> >SEGURO DE CRÉDITO </option>
+        <option value="transporte_cabotaje"<?php if ($producto =="transporte_cabotaje") echo "selected" ?> >TRANSPORTE - CABOTAJE</option>
+        <option value="transporte_intern"<?php if ($producto =="transporte_intern") echo "selected" ?> >TRANSPORTE - INTERNACIONAL </option>
+        <option value="vida_apv"<?php if ($producto =="vida_apv") echo "selected" ?> >VIDA - APV</option>
+        <option value="vida_vida"<?php if ($producto =="vida_vida") echo "selected" ?> >VIDA - VIDA</option>
+        <option value="vehiculo" <?php if ($producto == "vehiculo") echo "selected" ?> >Vehiculo</option>
+        <option value="hogar_persona" <?php if ($producto == "hogar_persona") echo "selected" ?> >Hogar</option>
+        <option value="hogar_pyme" <?php if ($producto == "hogar_pyme") echo "selected" ?> >PyME</option>
+        <option value="viaje" <?php if ($producto == "viaje") echo "selected" ?> >A. VIAJE</option>
+        <option value="rc_do" <?php if ($producto == "rc_do") echo "selected" ?> >RC - D&O</option>
+        <option value="inc" <?php if ($producto == "inc") echo "selected" ?> >INC</option>
+        <option value="apv" <?php if ($producto == "apv") echo "selected" ?> >APV</option>
+        <option value="ap" <?php if ($producto == "ap") echo "selected" ?> >AP</option>
+        <option value="vida" <?php if ($producto == "vida") echo "selected" ?> >Vida</option>
+        <option value="garantia" <?php if ($producto == "garantia") echo "selected" ?> >Garantía</option>
 
-<br>
+        </select>
+  
+  </div>
+  
+  <div class="col-3">
+  <label><b>Instancia</b></label>
+     <select class="form-control" name="instancia2" id="instancia2" onchange ="copia_instancia()">
+          <option value="null">Selecciona una Instancia</option>
+          <option >-------------------------------</option>
+          <option value="info_cotizar" <?php if ($instancia == "info_cotizar") echo "selected" ?>>Información para Cotizar</option>
+          <option value="envio_cotizacion" <?php if ($instancia == "envio_cotizacion") echo "selected" ?>>Envío de Cotización</option>
+ </select>
+  </div>
+  
+    <div class="col">
+   <button class="btn" type="submit" name="buscar"
+                        style="background-color: #536656; color: white; height: 45; align-self: end" onclick="envio_data(this.name)">Buscar
+    template</button>
+    
+ </div>
+ 
+
+</div>
 <div name='correo'>
-<div class=col>
+ <br>
+<br>   
+<div class="col-12">
   <h6>Resultado</h6>
   <div id="template_correo" class="form-control bg-light text-dark" rows="10"
-                style="height: 400px; border-style: solid;overflow-y: scroll"><?php echo $template; ?></div>
+                style="height: 300px; width:73vw; border-style: solid;overflow-y: scroll"><?php echo $template; ?></div>
   <br>
   <a class="btn" type="btn"
                         style="background-color: #536656; color: white; height: 45; align-self: center;" href="<?php echo urldecode($url); ?>" target="_blank">Enviar mail</a><br>
@@ -162,34 +220,37 @@ $url = htmlspecialchars( "https://mail.google.com/mail/?view=cm&fs=1&to=$destina
     </script>
 <script src="/assets/js/jquery.redirect.js"></script>
 
+</div>
+
+
+ </form>
+ </div>
 </body>
 
+
+</html>
+
 <script>
-
-
 function envio_data(boton) {
     document.getElementById("tipo").value = boton;
     
 }
 
 function copia_ramo(){
-    document.getElementById("ramo2").value = document.getElementById("ramo").value;
+    document.getElementById("ramo").value = document.getElementById("ramo2").value;
+}
+
+function copia_instancia(){
+    document.getElementById("instancia").value = document.getElementById("instancia2").value;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
 
-
-
+document.getElementById("instancia").value = '<?php echo $instancia; ?>';
 document.getElementById("ramo").value = '<?php echo $ramo_poliza; ?>';
+document.getElementById("instancia2").value = '<?php echo $instancia; ?>';
+document.getElementById("ramo2").value = '<?php echo $ramo_poliza; ?>';
 
 });
-
-</script>
-</div>
- </form>
- </div>
-</html>
-
-<script>
 
 </script>
