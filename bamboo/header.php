daafda902164592f40509e8427aaad93822fa3d8
IPNConsultores <transbank01>
2020-07-28 22:49:06 -0400

    header volver atras

--- bamboo/header.php
+++ bamboo/header.php
@@ -1,13 +1,18 @@
 <?php
 // Initialize the session
-    if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    } 
-// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION)) 
+{ 
+    session_start(); 
+} 
 if ( !isset( $_SESSION[ "loggedin" ] ) || $_SESSION[ "loggedin" ] !== true ) {
-  header( "location: /backend/login/login.php" );
-  exit;
+    setcookie('URI',$_SERVER['REQUEST_URI'],time() + (180),"/");
+    setcookie('DOMINIO',$_SERVER['HTTP_HOST'],time() + (180),"/");
+header( "location: /backend/login/login.php" );
+exit;
+}
+
+function valida_rut(){
+
 }
 ?>
 <!DOCTYPE html>
@@ -17,70 +22,184 @@ if ( !isset( $_SESSION[ "loggedin" ] ) || $_SESSION[ "loggedin" ] !== true ) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gestión IPN - Bamboo Seguros</title>
     <link rel="icon" href="/bamboo/images/bamboo.png">
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <!-- Bootstrap -->
-    <link href="css/bootstrap-4.3.1.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
-
 <body>
     <!-- body code goes here -->
     <div class="container">
-        <div class="card-header-tabs-p10" style="overflow:auto;  background-color: #536656;vertical-align: middle;">
+        <div class="card-header-tabs-p10" style="background-color:#536656;vertical-align: middle;">
             <div class="form-inline">
-                <p><img src="/bamboo/images/logo_bamboo.png" width="100" class="img-fluid"
-                        style="float: left;vertical-align: middle "></p>
-                <p class="h2"
-                    style="color:white; text-align: left;vertical-align: middle; font-family:'Varela Round', sans-serif;margin-left: 10px;">
-                    &nbsp;Gestión Bamboo</p>
+                <div class="col-2">
+                    <p><img src="/bamboo/images/logo_bamboo.png" width="100" class="img-fluid"
+                            style="float: left;vertical-align: middle "></p>
+                </div>
+                <div class="col-8">
+                    <p class="h2"
+                        style="color:white; text-align: left;vertical-align: middle; font-family:'Varela Round', sans-serif;margin-left: 10px;">
+                        &nbsp;Gestión Bamboo</p>
+                </div>
+                <div class="col-2" style="color : white; font-size:80%">
+                    <table id="ind_ec">
+                        <tr>
+                            <td width="100">Fecha :</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>UF:</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>Dólar:</td>
+                            <td>-</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
-        <nav class="navbar navbar-expand navbar-light shadow p-1" style="background-color: #A5CCAB; OVERFLOW=AUTO">
+        <nav class="navbar navbar-expand-lg navbar-light shadow p1" style="background-color: #A5CCAB">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span
                     class="navbar-toggler-icon"></span> </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active"> <a class="nav-link" type="button" href="/bamboo/index.php">Inicio <span
-                                class="sr-only">(current)</span> </a> </li>
-                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+                    <li class="nav-item active"> <a class="nav-link" href="/bamboo/index.php">Inicio <span
+                                class="sr-only">(current)</span></a> </li>
+                    <li class="dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Clientes </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" type="button" href="/bamboo/creacion_cliente.php">Creación</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown"> <a class="dropdown-item"
+                                type="button" href="/bamboo/creacion_cliente.php">Creación</a>
                             <a class="dropdown-item" type="button" href="/bamboo/listado_clientes.php"
-                                onclick="<?php $_SESSION["auxiliar"]=true;?>">Modificación</a> </div>
+                                onclick="<?php $_SESSION["auxiliar"]="header";?>">Listado de clientes</a> </div>
+                    </li>
+                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2"
+                            role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Pólizas
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="/bamboo/creacion_poliza.php">Creación</a>
+                            <a class="dropdown-item" href="/bamboo/listado_polizas.php">Listado de pólizas</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown4"
+                            role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tareas</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="/bamboo/creacion_actividades.php">Creación</a>
+                            <a class="dropdown-item" href="/bamboo/listado_tareas.php">Listado de tareas</a>
+                            <a class="dropdown-item" href="/bamboo/listado_tareas_recurrentes.php">Listado de tareas
+                                recurrentes</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown3"
+                            role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Correos</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="/bamboo/solicitar_info.php">Ver Tipos de Correos</a>
+                            <a class="dropdown-item" href="/bamboo/creacion_template.php">Editar Templates</a>
+                        </div>
                     </li>
                 </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2"
+                                role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Bienvenido, <?php echo htmlspecialchars($_SESSION["username"]); ?>
+                                <Usuario>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown"> <a class="dropdown-item"
+                                    href="/backend/login/registro.php"><i class="icon-cog"></i>Crear Nuevo usuario</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="/backend/login/logout.php"><i class="icon-off"></i>Cerrar
+                                    Sesión</a>
+                            </div>
+                        </li>
+                    </ul>
+                </form>
+                <form class="form-inline" action="/bamboo/resumen2.php" method="POST">
+                    <input class="form-control mr-sm-2" name="busqueda" type="text" placeholder="Buscar"
+                        aria-label="Buscar">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit"
+                        style="background-color:#536656;color:#A5CCAB"
+                        onclick="<?php $_SESSION["auxiliar"]="buscador_header";?>">Buscar</button>
+                </form>
+
             </div>
-            <ul class="navbar-nav mr-auto">
-                <form class="form-inline">
-                    <li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" style="color:black"
-                            data-toggle="dropdown">Bienvenido!, <?php echo htmlspecialchars($_SESSION["username"]); ?>
-                            <Usuario>
-                        </a>
-                        <ul class="nav-item dropdown-menu">
-                            <li><a href="/backend/login/registro.php"><i class="icon-cog"></i> Crear nuevo usuario</a>
-                            </li>
-                            <li class="divider"></li>
-                            <li><a href="/backend/login/logout.php"><i class="icon-off"></i> Cerrar Sesión</a></li>
-                        </ul>
-                    </li>
-                </form>
-                <form class="form-inline" method="POST" action="/bamboo/listado_clientes.php">
-                    <input class="form-control" name="busqueda" type="text" placeholder="Buscar" aria-label="Buscar">
-                    <button class="btn btn-outline-success " type="submit"
-                        style="background-color:#536656;color:#A5CCAB ">Buscar</button>
-                </form>
-            </ul>
         </nav>
+        <div>
+            <div class ="row d-flex">
+                <div class="col ">
+     
+    </div>
+    <div class="col">
+      
+    </div>
+    <div class="col ml-auto align-self-end" style="text-align: right">
+        
+      <button class="btn" style="vertical-align: right; background-color: transparent" ><img class="button" src="/bamboo/images/flecha_atras.png" width="30" class="img-fluid"
+                            style="float: right;vertical-align: right; margin-top:20px; margin-right:20px; " Onclick="volveratras()"></button>
+    </div>
+                
+            </div>
+            
+            
+        </div>
     </div>
     <br>
     <div class="needs-validation" novalidate>
         <form id="load" class="needs-validation" novalidate>
         </form>
     </div>
+    <script>
+    
+    function volveratras() {
+    window.history.back();
+    }
+    
+    </script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!--    <script src="js/jquery-3.3.1.min.js"></script>-->
+    <!-- onclick="?php $_SESSION["auxiliar"]='buscador';?" -->
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!--
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap-4.3.1.js"></script>
+-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
+<script>
+function formateo_numeros(x) {
+    return x.toString().replace(".", ",").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+var request = new XMLHttpRequest()
+request.open('GET', 'https://mindicador.cl/api', true)
+request.onload = function() {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+    if (request.status >= 200 && request.status < 400) {
+        let date = new Date(data.fecha)
+        let day = date.getDate()
+        let month = date.getMonth() + 1
+        let year = date.getFullYear()
+        if (month < 10) {
+            var fecha = `${day}/0${month}/${year}`
+        } else {
+            var fecha = `${day}/${month}/${year}`
+        }
+        document.getElementById('ind_ec').rows[0].cells[1].innerHTML = fecha;
+        document.getElementById('ind_ec').rows[1].cells[1].innerHTML = "$" + formateo_numeros(data.uf.valor);
+        document.getElementById('ind_ec').rows[2].cells[1].innerHTML = "$" + formateo_numeros(data.dolar.valor);
+    } else {}
+}
+request.send()
+</script>
