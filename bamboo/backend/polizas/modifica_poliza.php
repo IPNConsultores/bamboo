1b08a439e875420d90923bef25130041fc2b8c9a
IPNConsultores <transbank01>
2020-08-08 12:32:34 -0400

    cnaranjom 08082020 V1

--- bamboo/backend/polizas/modifica_poliza.php
+++ bamboo/backend/polizas/modifica_poliza.php
@@ -75,7 +75,7 @@ switch ($_POST["accion"]) {
         prima_bruta_anual='".$prima_bruta."',  monto_asegurado='".$monto_aseg."',  numero_propuesta='".$nro_propuesta."',  fecha_envio_propuesta='".$fechaprop."',  
         moneda_comision='".$moneda_comision."',  comision='".$comision."',  porcentaje_comision='".$porcentaje_comsion."',  comision_bruta='".$comisionbruta."',
         comision_neta='".$comisionneta."',  forma_pago='".$modo_pago."', nro_cuotas='".$cuotas."',  valor_cuota='".$valorcuota."',  fecha_primera_cuota='".$fechaprimer."', 
-        vendedor='".$con_vendedor."', nombre_vendedor='".$nombre_vendedor."', endoso='".$endoso."' , informacion_adicional='".$comentario."', venc_gtia ='".$venc_gtia."',fech_cancela = '".$fech_cancela."',motivo_cancela = '".$motivo_cancela."',  WHERE id=".$id_poliza.";";
+        vendedor='".$con_vendedor."', nombre_vendedor='".$nombre_vendedor."', endoso='".$endoso."' , informacion_adicional='".$comentario."', venc_gtia ='".$venc_gtia."',fech_cancela = '".$fech_cancela."',motivo_cancela = '".$motivo_cancela."'  WHERE id=".$id_poliza.";";
         mysqli_query($link, $query);
         mysqli_query($link, "select trazabilidad('".$_SESSION["username"]."', 'Actualiza póliza', '".str_replace("'","**",$query)."','poliza',".$_POST["id_poliza"].", '".$_SERVER['PHP_SELF']."')");
          break;
