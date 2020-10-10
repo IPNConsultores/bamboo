cc97679600b8ac12ef1a57392420132c799ca2a2
IPNConsultores <60800929+IPNConsultores@users.noreply.github.com>
2020-10-10 15:13:23 -0300

    Update index.php
    arreglo tareas index

--- bamboo/index.php
+++ bamboo/index.php
@@ -921,11 +921,14 @@ function botones(id, accion, base) {
                         accion:accion,
                     },
                 });
+               // table_tareas.clear();
+               // table_tareas.ajax.reload(null, false );
+               // table_tareas.draw();
+                //$('#tareas_completas').DataTable().ajax.reload(null, false );
+                alert('Tarea cerrada correctamente');
                 table_tareas.clear();
                 table_tareas.ajax.reload(null, false );
                 table_tareas.draw();
-                //$('#tareas_completas').DataTable().ajax.reload(null, false );
-                alert('Tarea cerrada correctamente');
             }
             break;
         }
