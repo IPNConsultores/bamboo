de2058caca5159ba7e84a1f054ebc2ca82a14280
Hatt3rPi <fabarca212@gmail.com>
2021-09-08 09:59:46 -0300

    BE listado de propuestas

--- bambooQA/listado_propuesta_polizas.php
+++ bambooQA/listado_propuesta_polizas.php
@@ -74,9 +74,6 @@ $buscar= estandariza_info($_POST["busqueda"]);
                     <th>Observaciones / Materia Asegurada</th>
                     <th>Proponente</th>
                     <th>Asegurado</th>
-                    <th>Póliza Renovada</th>
-                    <th>Información Adicional</th>
-                    <th>Fecha vencimiento Garantía</th>
                    
                     </tr>
                 </thead>
@@ -112,6 +109,396 @@ $buscar= estandariza_info($_POST["busqueda"]);
 
 </html>
 <script>
+var table = ''
+$(document).ready(function() {
+    table = $('#listado_propuesta_polizas').DataTable({
+        "ajax": "/bambooQA/backend/propuesta_polizas/busqueda_listado_propuesta_polizas.php",
+        "scrollX": true,
+        "searchPanes":{
+            "columns":[2,3,13,14],
+        },
+        "dom": 'Pfrtip',
+        "columns": [{
+                "className": 'details-control',
+                "orderable": false,
+                "data": null,
+                "defaultContent": '<i class="fas fa-search-plus"></i>'
+            },
+            {
+                "data": "estado",
+                title: "Estado"
+            },
+            { 
+                data: null, 
+                title: "Nro Propuesta",
+                render: function ( data, type, row ) {
+                    return data.numero_propuesta + ' (' + data.item + ')';
+            } },
+            {
+                "data": "compania",
+                title: "Compañía"
+            },
+            {
+                "data": "ramo",
+                title: "Ramo"
+            },
+            {
+                "data": "vigencia_inicial",
+                title: "Vigencia Inicio"
+            },
+            {
+                "data": "vigencia_final",
+                title: "Vigencia Término"
+            },
+            {
+                "data": "materia_asegurada",
+                title: "Materia asegurada"
+            },
+            {
+                "data": "tipo_poliza",
+                title: "Tipo póliza"
+            },
+            {
+                "data": "patente_ubicacion",
+                title: "Observaciones materia asegurada"
+            },
+            {
+                "data": "deducible",
+                title: "Deducible"
+            },
+            {
+                "data": "prima_afecta",
+                title: "Prima afecta"
+            },
+            {
+                "data": "prima_exenta",
+                title: "Prima exenta"
+            },
+            {
+                "data": "prima_bruta_anual",
+                title: "Prima bruta anual"
+            },
+            {
+                "data": "anomes_final",
+                title: "Añomes final"
+            },
+            {
+                "data": "anomes_inicial",
+                title: "Añomes inicial"
+            },
+            {
+                "data": "moneda_poliza",
+                title: "Moneda póliza"
+            },
+            {
+                "data": "cobertura",
+                title: "Cobertura"
+            },
+            {
+                "data": "nom_clienteP",
+                title: "Proponente"
+            },
+            {
+                "data": "rut_clienteP",
+                title: "Rut Proponente"
+            },
+            {
+                "data": "nom_clienteA",
+                title: "Asegurado"
+            },
+            {
+                "data": "rut_clienteA",
+                title: "Rut Asegurado"
+            },
+            {
+                "data": "grupo",
+                title: "Grupo"
+            },
+            {
+                "data": "referido",
+                title: "Referido"
+            },
+            {
+                "data": "monto_asegurado",
+                title: "Monto Asegurado"
+            },
+            {
+                "data": "fecha_envio_propuesta",
+                title: "Fecha envío propuesto"
+            }
+        ],
+        //          "search": {
+        //          "search": "abarca"
+        //          },
+        "columnDefs": [{
+                "targets": [10, 11, 12,13,14,15,16,17,19,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,45,46],
+                "visible": false,
+            },
+            {
+                "targets": [10, 11, 12,13,14,15,16,17,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,45,46,44],
+                "searchable": false
+            },
+            {
+        targets: 1,
+        render: function (data, type, row, meta) {
+             var estado='';
+            switch (data) {
+                        case 'Activo':
+                            estado='<span class="badge badge-primary">'+data+'</span>';
+                            break;
+                        case 'Renovado':
+                                estado='<span class="badge badge-warning">'+data+'</span>';
+                                break;
+                        case 'Vencido':
+                            estado='<span class="badge badge-danger">'+data+'</span>';
+                            break;
+                        case 'Cancelado':
+                            estado='<span class="badge badge-dark">'+data+'</span>';
+                            break;
+                        default:
+                            estado='<span class="badge badge-light">'+data+'</span>';
+                            break;
+                    }
+          return estado;  //render link in cell
+        }},
+        {
+        targets: [5,6,45,44],
+         render: function(data, type, full)
+         {
+             if (type == 'display')
+                 return moment(data).format('DD/MM/YYYY');
+             else
+                 return moment(data).format('YYYY/MM/DD');
+         }}
+        ],
+        "order": [
+            [3, "asc"],
+            [4, "asc"]
+        ],
+        "oLanguage": {
+            "sSearch": "Búsqueda rápida",
+            "sLengthMenu": 'Mostrar <select>' +
+                '<option value="10">10</option>' +
+                '<option value="25">30</option>' +
+                '<option value="50">50</option>' +
+                '<option value="-1">todos</option>' +
+                '</select> registros',
+                "sInfoFiltered": "(Resultado búsqueda: _TOTAL_ de _MAX_ registros totales)",
+            "sLengthMenu": "Muestra _MENU_ registros por página",
+            "sZeroRecords": "No hay registros asociados",
+            "sInfo": "Mostrando página _PAGE_ de _PAGES_",
+            "sInfoEmpty": "No hay registros disponibles",
+            "oPaginate": {
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior",
+                "sLast": "Última"
+            }
+        },
+        "language": {
+            "searchPanes": {
+                "title":{
+                    _: 'Filtros seleccionados - %d',
+                    0: 'Sin Filtros Seleccionados',
+                    1: '1 Filtro Seleccionado',
+                }
+            }
+        }
+    });
+    $("#listado_propuesta_polizas_filter input")
+    .off()
+    .on('keyup change', function (e) {
+    if (e.keyCode !== 13 || this.value == "") {
+        var texto1=this.value.normalize("NFD").replace(/[\u0300-\u036f]/g, "");  
+         table.search(texto1)
+            .draw();
+    }
+        
+    });
+    $('#listado_propuesta_polizas tbody').on('click', 'td.details-control', function() {
+        var tr = $(this).closest('tr');
+        var row = table.row(tr);
 
+        if (row.child.isShown()) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        } else {
+            // Open this row
+            row.child(format(row.data())).show();
+            tr.addClass('shown');
+        }
+    });
+    $('#listado_propuesta_polizas').dataTable().fnFilter(document.getElementById("var1").value);
+    var dd = new Date();
+    var fecha = '' + dd.getFullYear() + '-' + (("0" + (dd.getMonth() + 1)).slice(-2)) + '-' + (("0" + (dd
+        .getDate() + 1)).slice(-2)) + ' (' + dd.getHours() + dd.getMinutes() + dd.getSeconds() + ')';
 
+    var buttons = new $.fn.dataTable.Buttons(table, {
+        buttons: [{
+                sheetName: 'Propuestas de Pólizas',
+                orientation: 'landscape',
+                extend: 'excelHtml5',
+                filename: 'Listado Propuestas de Pólizas al: ' + fecha,
+                exportOptions: {
+                    columns: [1,18,19,20,21,22,3,5,6,14,8,4,2,7,9,17,16,10,11,12,41,13,24,25,26,27,28,29,30,31,33,32,34,35,23,37,38,39,40,42,43,44,45,46]
+                }
+            },
+            {
+                orientation: 'landscape',
+                extend: 'pdfHtml5',
+                filename: 'Listado Propuestas de Pólizas al: ' + fecha,
+                exportOptions: {
+                    columns: [1,18,19,20,21,22,3,5,6,14,8,4,2,7,9,17,16,10,11,12,41,13,24,25,26,27,28,29,30,31,33,32,34,35,23,37,38,39,40,42,43,44,45,46]
+                }
+            }
+        ]
+    }).container().appendTo($('#botones_poliza'));
+
+});
+
+function format(d) {
+    // `d` is the original data object for the row
+    var ext_cancelado='';
+    if (d.estado=='Cancelado'){
+        ext_cancelado='<tr>' +
+        '<td>Fecha CANCELACIÓN:</td>' +
+        '<td>' + d.fech_cancela + '</td>' +
+        '</tr>'+
+        '<tr>' +
+        '<td>motivo CANCELACIÓN:</td>' +
+        '<td>' + d.motivo_cancela + '</td>' +
+        '</tr>';
+    }
+    return '<table background-color:#F6F6F6; color:#FFF; cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+        '<tr>' +
+        '<td>Deducible:</td>' +
+        '<td>' + d.deducible +'</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Prima afecta:</td>' +
+        '<td>' + d.prima_afecta + '</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Prima exenta:</td>' +
+        '<td>' + d.prima_exenta + '</td>' +
+        '</tr>' +
+        ext_cancelado + 
+        '<tr>' +
+        '<td>Prima bruta anual:</td>' +
+        '<td>' + d.prima_bruta_anual + '</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Acciones</td>' +
+        '<td><button title="Buscar información asociada" type="button" id=' + d.id_poliza +
+        ' name="info" onclick="botones(this.id, this.name, \'poliza\')"><i class="fas fa-search"></i></button><a> </a><button title="Editar"  type="button" id=' +
+        d.id_poliza +
+        ' name="modifica" onclick="botones(this.id, this.name, \'poliza\')"><i class="fas fa-edit"></i></button><a> </a><button title="Asignar tarea"  type="button" id=' +
+        d.id_poliza +
+        ' name="tarea" onclick="botones(this.id, this.name, \'poliza\')"><i class="fas fa-clipboard-list"></i></button><a> </a><button title="Generar correo"  type="button"' +
+        'id='+ d.id_poliza +
+        ' name="correo" onclick="botones(this.id, this.name, \'poliza\')"><i class="fas fa-envelope-open-text"></i></button><a> </a><button style="background-color: #FF0000" title="Eliminar"  type="button" id=' +
+        d.id_poliza +
+        ' name="elimina" onclick="botones(this.id, this.name, \'poliza\')"><i class="fas fa-trash-alt"></i></button></td>' +
+
+        '</tr>' +
+        '</table>';
+}
+
+function botones(id, accion, base) {
+    console.log("ID:" + id + " => acción:" + accion);
+    switch (accion) {
+        case "elimina": {
+
+            if (base == 'poliza') {
+                var r2 = confirm("Estás a punto de eliminar está póliza ¿Deseas continuar?");
+                if (r2 == true) {
+                $.redirect('/bambooQA/backend/propuesta_polizas/modifica_propuesta_poliza.php', {
+                    'id_poliza': id,
+                    'accion':accion,
+                }, 'post');
+                }
+            }
+            break;
+        }
+        case "modifica": {
+            if (base == 'poliza') {
+                $.redirect('/bambooQA/creacion_propuesta_poliza.php', {
+                'id_propuesta': id,
+                }, 'post');
+            }
+            console.log(base + " modificado con ID:" + id);
+            $.notify({
+                // options
+                message: base + ' modificado'
+            }, {
+                // settings
+                type: 'success'
+            });
+
+            break;
+        }
+        case "tarea": {
+            if (base == 'cliente') {
+                $.redirect('/bambooQA/creacion_actividades.php', {
+                    'id_cliente': id
+                }, 'post');
+            }
+            if (base == 'propuesta'){
+                $.redirect('/bambooQA/creacion_actividades.php', {
+                    'id_propuesta': id
+                }, 'post');
+            }
+            break;
+        }
+        case "info": {
+            $.redirect('/bambooQA/resumen2.php', {
+                'id': id,
+                'base': base
+            }, 'post');
+            break;
+        }
+        case "correo": {
+            if (base == 'propuesta'){
+                $.redirect('/bambooQA/template_poliza.php', {
+                    'id_propuesta': id
+                }, 'post');
+            }
+            break;
+        }
+    }
+}
+(function(){
+ 
+ function removeAccents ( data ) {
+     if ( data.normalize ) {
+         // Use I18n API if avaiable to split characters and accents, then remove
+         // the accents wholesale. Note that we use the original data as well as
+         // the new to allow for searching of either form.
+         return data +' '+ data
+             .normalize('NFD')
+             .replace(/[\u0300-\u036f]/g, '');
+     }
+  
+     return data;
+ }
+  
+ var searchType = jQuery.fn.DataTable.ext.type.search;
+  
+ searchType.string = function ( data ) {
+     return ! data ?
+         '' :
+         typeof data === 'string' ?
+             removeAccents( data ) :
+             data;
+ };
+  
+ searchType.html = function ( data ) {
+     return ! data ?
+         '' :
+         typeof data === 'string' ?
+             removeAccents( data.replace( /<.*?>/g, '' ) ) :
+             data;
+ };
+  
+ }());
 </script>
