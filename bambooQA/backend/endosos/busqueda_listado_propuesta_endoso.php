f8965bf462626ecb265932662fc717e22bca9526
H4tt3r <fabarca212@gmail.com>
2022-05-19 22:19:51 -0400

    correccion busqueda y creación de propuesta de endoso

--- bambooQA/backend/endosos/busqueda_listado_propuesta_endoso.php
+++ bambooQA/backend/endosos/busqueda_listado_propuesta_endoso.php
@@ -3,116 +3,60 @@
     { 
         session_start(); 
     } 
+$resultado =$codigo=$conta='';
 
 require_once "/home/gestio10/public_html/backend/config.php";
-mysqli_set_charset( $link, 'utf8');
-mysqli_select_db($link, 'gestio10_asesori1_bamboo_QA');
-$mensaje='';
-$busqueda='';
-$listado='';
-if ($_SERVER[ "REQUEST_METHOD" ] == "POST")
+
+    mysqli_set_charset($link, 'utf8');
+    mysqli_select_db($link, 'gestio10_asesori1_bamboo_QA');
+    $sql = "SELECT estado, tipo_endoso, compania,fecha_ingreso_endoso, ramo, vigencia_inicial, vigencia_final, numero_poliza, numero_propuesta_endoso, CONCAT_WS(' ',moneda_poliza_endoso,FORMAT(prima_neta_afecta, 2, 'de_DE')) as prima_neta_afecta, CONCAT_WS(' ',moneda_poliza_endoso,FORMAT(iva, 2, 'de_DE')) as iva, CONCAT_WS(' ',moneda_poliza_endoso,FORMAT(prima_neta_exenta, 2, 'de_DE')) as prima_neta_exenta, CONCAT_WS(' ',moneda_poliza_endoso,FORMAT(prima_total, 2, 'de_DE')) as prima_total, dice, debe_decir, descripcion_endoso FROM propuesta_endosos as a where a.estado <> 'Rechazado'";
+    $resultado=mysqli_query($link, $sql);
+    $codigo='{
+      "data": [';
+    $conta=0;
+    While($row=mysqli_fetch_object($resultado))
     {
-        $tipo_endoso=cambia_puntos_por_coma(estandariza_info($_POST["tipo_endoso"]));
-        $ramo=estandariza_info($_POST["ramo"]);
-        $compania=estandariza_info($_POST["compania"]);
-        $nro_poliza=estandariza_info($_POST["nro_poliza"]);
-        $id_poliza=estandariza_info($_POST["id_poliza"]);
-        $fecha_ingreso=estandariza_info($_POST["fecha_ingreso"]);
-        $fecha_vigencia_inicial=estandariza_info($_POST["fecha_vigencia_inicial"]);
-        $fecha_vigencia_final=estandariza_info($_POST["fecha_vigencia_final"]);
-        $dias=estandariza_info($_POST["dias"]);
-        $rut_completo_prop = str_replace("-", "", estandariza_info($_POST["rutprop"]));
-        $rut_prop=estandariza_info(substr($rut_completo_prop, 0, strlen($rut_completo_prop)-1));
-        $dv_prop=estandariza_info(substr($rut_completo_prop, -1,1));
-        $nombre=estandariza_info($_POST["nombre"]);
-        $descripcion_endoso=cambia_puntos_por_coma(estandariza_info($_POST["descripcion_endoso"]));
-        $dice=cambia_puntos_por_coma(estandariza_info($_POST["dice"]));
-        $debe_decir=cambia_puntos_por_coma(estandariza_info($_POST["debe_decir"]));
-        $monto=cambia_puntos_por_coma(estandariza_info($_POST["monto"]));
-        $moneda_poliza=estandariza_info($_POST["moneda_poliza"]);
-        $prima_neta_exenta=cambia_puntos_por_coma(estandariza_info($_POST["prima_neta_exenta"]));
-        $iva=cambia_puntos_por_coma(estandariza_info($_POST["iva"]));
-        $prima_neta_afecta=cambia_puntos_por_coma(estandariza_info($_POST["prima_neta_afecta"]));
-        $prima_total=cambia_puntos_por_coma(estandariza_info($_POST["prima_total"]));
-        $tasa_afecta=cambia_puntos_por_coma(estandariza_info($_POST["tasa_afecta"]));
-        $tasa_exenta=cambia_puntos_por_coma(estandariza_info($_POST["tasa_exenta"]));
-            
-    switch ($_POST["accion"]){
-        case 'crea_propuesta_endoso':
-            $listado='listado_propuesta_endosos.php';
-            $mensaje='Propuesta de endoso creada correctamente';
-            $largo = 6;
-            $token = bin2hex(random_bytes($largo));
-            $query="insert into propuesta_endosos(tasa_afecta_endoso, tasa_exenta_endoso, estado, tipo_endoso, ramo, numero_poliza, fecha_ingreso_endoso, vigencia_inicial, vigencia_final, rut_proponente, dv_proponente, nombre_proponente, descripcion_endoso, dice, debe_decir, monto_asegurado_endoso, moneda_poliza_endoso, prima_neta_exenta, IVA, prima_neta_afecta, prima_total, token, id_poliza, compania) values ('".$tasa_afecta."','".$tasa_exenta."','Emitido','".$tipo_endoso."','".$ramo."',  '".$nro_poliza."','".$fecha_ingreso."','".$fecha_vigencia_inicial."','".$fecha_vigencia_final."','".$rut_prop."','".$dv_prop."','".$nombre."','".$descripcion_endoso."','".$dice."','".$debe_decir."','".$monto."','".$moneda_poliza."','".$prima_neta_exenta."','".$iva."','".$prima_neta_afecta."','".$prima_total."','".$token."','".$id_poliza."','".$compania."')";
-            mysqli_query($link, $query);
-            mysqli_query($link, 'update propuesta_endosos set numero_propuesta_endoso=CONCAT(\'E\', LPAD(id,6,0)) where token=\'' . $token . '\';');
-            $resultado = mysqli_query($link, 'select id, numero_propuesta_endoso from propuesta_endosos where token=\'' . $token . '\';');
-            while ($fila = mysqli_fetch_object($resultado))
-            {
-                // printf ("%s (%s)\n", $fila->id);
-                $id_poliza = $fila->id;
-                $numero_propuesta_endoso = $fila->numero_propuesta_endoso;
-            }
-            mysqli_query($link, "select trazabilidad('".$_SESSION["username"]."', 'Creación propuesta endoso', '".str_replace("'","**",$query)."','propuesta_endoso', '".$numero_propuesta_endoso."' , '".$_SERVER['PHP_SELF']."')");
-            $busqueda=$numero_propuesta_endoso;
-            break;
-        case 'actualiza_propuesta':
-            $listado='listado_propuesta_endosos.php';
-            $mensaje='Propuesta de endoso actualizada correctamente';
-            $query="";
-            mysqli_query($link, $query);
-            mysqli_query($link, "select trazabilidad('".$_SESSION["username"]."', 'Modificación propuesta endoso', '".str_replace("'","**",$query)."','propuesta_endoso', '".$numero_propuesta_endoso."' , '".$_SERVER['PHP_SELF']."')");
-            $busqueda=$numero_propuesta_endoso;
-            break;
-        case 'crear_endoso':
-            $listado='listado_endosos.php';
-            $mensaje='Endoso creado correctamente';
-            $query="";
-            mysqli_query($link, $query);
-            mysqli_query($link, "select trazabilidad('".$_SESSION["username"]."', 'Creación endoso', '".str_replace("'","**",$query)."','endoso', '".$numero_endoso."' , '".$_SERVER['PHP_SELF']."')");
-            $busqueda=$numero_propuesta_endoso;
-            break;
+        $conta=$conta+1;
+        if ($conta==1){
+            $codigo.= json_encode(array(
+            "estado" =>& $row->estado,
+            "fecha_ingreso_endoso"=>& $row->fecha_ingreso_endoso,
+            "tipo_endoso"=>& $row->tipo_endoso,
+            "compania"=>& $row->compania,
+            "ramo" =>& $row->ramo,
+            "vigencia_inicial" =>& $row->vigencia_inicial,
+            "vigencia_final" =>& $row->vigencia_final,
+            "numero_poliza" =>& $row->numero_poliza,
+            "numero_propuesta_endoso" =>& $row->numero_propuesta_endoso,
+            "prima_neta_afecta" =>& $row->prima_neta_afecta,
+            "prima_neta_exenta" =>& $row->prima_neta_exenta,
+            "iva" =>& $row->iva,
+            "prima_total" =>& $row->prima_total,
+            "dice" =>& $row->dice,
+            "debe_decir" =>& $row->debe_decir,
+            "descripcion_endoso" =>& $row->descripcion_endoso));
+        } else {
+            $codigo.= ', '.json_encode(array(
+            "estado" =>& $row->estado,
+            "fecha_ingreso_endoso"=>& $row->fecha_ingreso_endoso,
+            "tipo_endoso"=>& $row->tipo_endoso,
+            "compania"=>& $row->compania,
+            "ramo" =>& $row->ramo,
+            "vigencia_inicial" =>& $row->vigencia_inicial,
+            "vigencia_final" =>& $row->vigencia_final,
+            "numero_poliza" =>& $row->numero_poliza,
+            "numero_propuesta_endoso" =>& $row->numero_propuesta_endoso,
+            "prima_neta_afecta" =>& $row->prima_neta_afecta,
+            "prima_neta_exenta" =>& $row->prima_neta_exenta,
+            "iva" =>& $row->iva,
+            "prima_total" =>& $row->prima_total,
+            "dice" =>& $row->dice,
+            "debe_decir" =>& $row->debe_decir,
+            "descripcion_endoso" =>& $row->descripcion_endoso));
+        }
     }
-}
+  $codigo.=']}';
+  mysqli_close($link);
+  echo $codigo;
 
-
-
-//echo $query;
-
-function estandariza_info($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-function cambia_puntos_por_coma($data){
-  //$data=str_replace('.','',$data);
-$data=str_replace(',','.',$data);
-return $data;
-}
-
-mysqli_close($link);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js">
-    </script>
-<script src="/assets/js/jquery.redirect.js">
-</script>
-</head>
-<body>
-<script >
-
-var mensaje= '<?php echo $mensaje; ?>';
-alert(mensaje);
-var busqueda= '<?php echo $busqueda; ?>';
-var listado= '<?php echo $listado; ?>';
-  $.redirect(listado, {
- 'busqueda': busqueda
-}, 'post');
-
-</script>
-</body>
-</html>
--- bambooQA/backend/endosos/crea_endosos.php
+++ bambooQA/backend/endosos/crea_endosos.php
@@ -10,12 +10,8 @@ mysqli_select_db($link, 'gestio10_asesori1_bamboo_QA');
 $mensaje='';
 $busqueda='';
 $listado='';
-echo 'inicio con acción: '.$_POST["accion"];
-if ($_SERVER[ "REQUEST_METHOD" ] == "POST" and $_POST["accion"] == 'crea_propuesta_endoso')
+if ($_SERVER[ "REQUEST_METHOD" ] == "POST")
     {
-        $listado='listado_propuesta_endosos.php';
-        $mensaje='Propuesta de endoso creada correctamente';
-        
         $tipo_endoso=cambia_puntos_por_coma(estandariza_info($_POST["tipo_endoso"]));
         $ramo=estandariza_info($_POST["ramo"]);
         $compania=estandariza_info($_POST["compania"]);
@@ -25,39 +21,59 @@ if ($_SERVER[ "REQUEST_METHOD" ] == "POST" and $_POST["accion"] == 'crea_propues
         $fecha_vigencia_inicial=estandariza_info($_POST["fecha_vigencia_inicial"]);
         $fecha_vigencia_final=estandariza_info($_POST["fecha_vigencia_final"]);
         $dias=estandariza_info($_POST["dias"]);
-
         $rut_completo_prop = str_replace("-", "", estandariza_info($_POST["rutprop"]));
         $rut_prop=estandariza_info(substr($rut_completo_prop, 0, strlen($rut_completo_prop)-1));
         $dv_prop=estandariza_info(substr($rut_completo_prop, -1,1));
         $nombre=estandariza_info($_POST["nombre"]);
-
         $descripcion_endoso=cambia_puntos_por_coma(estandariza_info($_POST["descripcion_endoso"]));
         $dice=cambia_puntos_por_coma(estandariza_info($_POST["dice"]));
         $debe_decir=cambia_puntos_por_coma(estandariza_info($_POST["debe_decir"]));
-
         $monto=cambia_puntos_por_coma(estandariza_info($_POST["monto"]));
         $moneda_poliza=estandariza_info($_POST["moneda_poliza"]);
         $prima_neta_exenta=cambia_puntos_por_coma(estandariza_info($_POST["prima_neta_exenta"]));
         $iva=cambia_puntos_por_coma(estandariza_info($_POST["iva"]));
         $prima_neta_afecta=cambia_puntos_por_coma(estandariza_info($_POST["prima_neta_afecta"]));
         $prima_total=cambia_puntos_por_coma(estandariza_info($_POST["prima_total"]));
-        $largo = 6;
-        $token = bin2hex(random_bytes($largo));
-        $query="insert into propuesta_endosos(estado, tipo_endoso, ramo, numero_poliza, fecha_ingreso_endoso, vigencia_inicial, vigencia_final, rut_proponente, dv_proponente, nombre_proponente, descripcion_endoso, dice, debe_decir, monto_asegurado_endoso, moneda_poliza_endoso, prima_neta_exenta, IVA, prima_neta_afecta, prima_total, token, id_poliza, compania) values ('Emitido','".$tipo_endoso."','".$ramo."',  '".$nro_poliza."','".$fecha_ingreso."','".$fecha_vigencia_inicial."','".$fecha_vigencia_final."','".$rut_prop."','".$dv_prop."','".$nombre."','".$descripcion_endoso."','".$dice."','".$debe_decir."','".$monto."','".$moneda_poliza."','".$prima_neta_exenta."','".$iva."','".$prima_neta_afecta."','".$prima_total."','".$token."','".$id_poliza."','".$compania."')";
-        
-        mysqli_query($link, $query);
-        mysqli_query($link, 'update propuesta_endosos set numero_propuesta_endoso=CONCAT(\'E\', LPAD(id,6,0)) where token=\'' . $token . '\';');
-        $resultado = mysqli_query($link, 'select id, numero_propuesta_endoso from propuesta_endosos where token=\'' . $token . '\';');
-        while ($fila = mysqli_fetch_object($resultado))
-        {
-            // printf ("%s (%s)\n", $fila->id);
-            $id_poliza = $fila->id;
-            $numero_propuesta_endoso = $fila->numero_propuesta_endoso;
-        }
-        mysqli_query($link, "select trazabilidad('".$_SESSION["username"]."', 'Creación propuesta endoso', '".str_replace("'","**",$query)."','propuesta_endoso', '".$numero_propuesta_endoso."' , '".$_SERVER['PHP_SELF']."')");
-        $busqueda=$numero_propuesta_endoso;
+        $tasa_afecta=cambia_puntos_por_coma(estandariza_info($_POST["tasa_afecta"]));
+        $tasa_exenta=cambia_puntos_por_coma(estandariza_info($_POST["tasa_exenta"]));
+            
+    switch ($_POST["accion"]){
+        case 'crea_propuesta_endoso':
+            $listado='listado_propuesta_endosos.php';
+            $mensaje='Propuesta de endoso creada correctamente';
+            $largo = 6;
+            $token = bin2hex(random_bytes($largo));
+            $query="insert into propuesta_endosos(tasa_afecta_endoso, tasa_exenta_endoso, estado, tipo_endoso, ramo, numero_poliza, fecha_ingreso_endoso, vigencia_inicial, vigencia_final, rut_proponente, dv_proponente, nombre_proponente, descripcion_endoso, dice, debe_decir, monto_asegurado_endoso, moneda_poliza_endoso, prima_neta_exenta, IVA, prima_neta_afecta, prima_total, token, id_poliza, compania) values ('".$tasa_afecta."','".$tasa_exenta."','Emitido','".$tipo_endoso."','".$ramo."',  '".$nro_poliza."','".$fecha_ingreso."','".$fecha_vigencia_inicial."','".$fecha_vigencia_final."','".$rut_prop."','".$dv_prop."','".$nombre."','".$descripcion_endoso."','".$dice."','".$debe_decir."','".$monto."','".$moneda_poliza."','".$prima_neta_exenta."','".$iva."','".$prima_neta_afecta."','".$prima_total."','".$token."','".$id_poliza."','".$compania."')";
+            mysqli_query($link, $query);
+            mysqli_query($link, 'update propuesta_endosos set numero_propuesta_endoso=CONCAT(\'E\', LPAD(id,6,0)) where token=\'' . $token . '\';');
+            $resultado = mysqli_query($link, 'select id, numero_propuesta_endoso from propuesta_endosos where token=\'' . $token . '\';');
+            while ($fila = mysqli_fetch_object($resultado))
+            {
+                // printf ("%s (%s)\n", $fila->id);
+                $id_poliza = $fila->id;
+                $numero_propuesta_endoso = $fila->numero_propuesta_endoso;
+            }
+            mysqli_query($link, "select trazabilidad('".$_SESSION["username"]."', 'Creación propuesta endoso', '".str_replace("'","**",$query)."','propuesta_endoso', '".$numero_propuesta_endoso."' , '".$_SERVER['PHP_SELF']."')");
+            $busqueda=$numero_propuesta_endoso;
+            break;
+        case 'actualiza_propuesta':
+            $listado='listado_propuesta_endosos.php';
+            $mensaje='Propuesta de endoso actualizada correctamente';
+            $query="";
+            mysqli_query($link, $query);
+            mysqli_query($link, "select trazabilidad('".$_SESSION["username"]."', 'Modificación propuesta endoso', '".str_replace("'","**",$query)."','propuesta_endoso', '".$numero_propuesta_endoso."' , '".$_SERVER['PHP_SELF']."')");
+            $busqueda=$numero_propuesta_endoso;
+            break;
+        case 'crear_endoso':
+            $listado='listado_endosos.php';
+            $mensaje='Endoso creado correctamente';
+            $query="";
+            mysqli_query($link, $query);
+            mysqli_query($link, "select trazabilidad('".$_SESSION["username"]."', 'Creación endoso', '".str_replace("'","**",$query)."','endoso', '".$numero_endoso."' , '".$_SERVER['PHP_SELF']."')");
+            $busqueda=$numero_propuesta_endoso;
+            break;
     }
-
+}
 
 
 
