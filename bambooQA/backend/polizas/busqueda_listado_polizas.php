7776c22ae0661b1a3ab98fd7930a37e9487eb6b8
H4tt3r <fabarca212@gmail.com>
2022-05-23 23:05:06 -0400

    incorporación endosos en listado pólizas

--- bambooQA/backend/polizas/busqueda_listado_polizas.php
+++ bambooQA/backend/polizas/busqueda_listado_polizas.php
@@ -3,14 +3,13 @@
     { 
         session_start(); 
     } 
-$resultado =$codigo=$conta='';
+$resultado =$codigo=$conta=$nro_endosos='';
 
 require_once "/home/gestio10/public_html/backend/config.php";
 
     mysqli_set_charset($link, 'utf8');
     mysqli_select_db($link, 'gestio10_asesori1_bamboo_QA');
-    $sql = "SELECT a.numero_poliza, estado, DATE_FORMAT(vigencia_final,'%m-%Y') as anomes_final, DATE_FORMAT(vigencia_inicial,'%m-%Y')  as anomes_inicial, moneda_poliza, compania, ramo,  vigencia_inicial, vigencia_final,  CONCAT_WS(' ',b.nombre_cliente,  b.apellido_paterno, ' ', b.apellido_materno) as nom_clienteP, CONCAT_WS('-',b.rut_sin_dv, b.dv) as rut_clienteP,b.telefono as telefonoP, b.correo as correoP, a.id as id_poliza, b.id as idP, fecha_envio_propuesta, b.grupo, b.referido, CONCAT_WS(' ',a.moneda_poliza,FORMAT(sum(c.prima_afecta), 2, 'de_DE')) as total_prima_afecta,  CONCAT_WS(' ',a.moneda_poliza,FORMAT(sum(c.prima_exenta), 2, 'de_DE')) as total_prima_exenta, CONCAT_WS(' ',a.moneda_poliza,FORMAT(sum(c.prima_neta), 2, 'de_DE')) as total_prima_neta, CONCAT_WS(' ',a.moneda_poliza,FORMAT(sum(c.prima_bruta_anual), 2, 'de_DE')) as total_prima_bruta  FROM polizas_2 as a left join clientes as b on a.rut_proponente=b.rut_sin_dv and b.rut_sin_dv is not null left join items as c on a.numero_poliza=c.numero_poliza where a.estado <> 'Rechazado' 
-group by a.numero_poliza, estado, DATE_FORMAT(vigencia_final,'%m-%Y') , DATE_FORMAT(vigencia_inicial,'%m-%Y') , moneda_poliza, compania, ramo,  vigencia_inicial, vigencia_final,   CONCAT_WS(' ',b.nombre_cliente,  b.apellido_paterno, ' ', b.apellido_materno) , CONCAT_WS('-',b.rut_sin_dv, b.dv) ,b.telefono , b.correo , a.id , b.id , fecha_envio_propuesta, b.grupo, b.referido  ";
+    $sql = "SELECT a.numero_poliza, a.estado, DATE_FORMAT(a.vigencia_final,'%m-%Y') as anomes_final, DATE_FORMAT(a.vigencia_inicial,'%m-%Y')  as anomes_inicial, a.moneda_poliza, a.compania, a.ramo,  a.vigencia_inicial, a.vigencia_final,  CONCAT_WS(' ',b.nombre_cliente,  b.apellido_paterno, ' ', b.apellido_materno) as nom_clienteP, CONCAT_WS('-',b.rut_sin_dv, b.dv) as rut_clienteP,b.telefono as telefonoP, b.correo as correoP, a.id as id_poliza, b.id as idP, fecha_envio_propuesta, b.grupo, b.referido, CONCAT_WS(' ',a.moneda_poliza,FORMAT(sum(c.prima_afecta), 2, 'de_DE')) as total_prima_afecta,  CONCAT_WS(' ',a.moneda_poliza,FORMAT(sum(c.prima_exenta), 2, 'de_DE')) as total_prima_exenta, CONCAT_WS(' ',a.moneda_poliza,FORMAT(sum(c.prima_neta), 2, 'de_DE')) as total_prima_neta, CONCAT_WS(' ',a.moneda_poliza,FORMAT(sum(c.prima_bruta_anual), 2, 'de_DE')) as total_prima_bruta, count(e.id) as contador_endosos FROM polizas_2 as a left join clientes as b on a.rut_proponente=b.rut_sin_dv and b.rut_sin_dv is not null left join items as c on a.numero_poliza=c.numero_poliza left join endosos as e on a.id=e.id_poliza where a.estado <> 'Rechazado' group by a.numero_poliza, a.estado, DATE_FORMAT(a.vigencia_final,'%m-%Y') , DATE_FORMAT(a.vigencia_inicial,'%m-%Y') , a.moneda_poliza, a.compania, a.ramo,  a.vigencia_inicial, a.vigencia_final,   CONCAT_WS(' ',b.nombre_cliente,  b.apellido_paterno, ' ', b.apellido_materno) , CONCAT_WS('-',b.rut_sin_dv, b.dv) ,b.telefono , b.correo , a.id , b.id , fecha_envio_propuesta, b.grupo, b.referido";
   
 $resultado=mysqli_query($link, $sql);
     $codigo='{
@@ -18,6 +17,7 @@ $resultado=mysqli_query($link, $sql);
     $conta=0;
   While($row=mysqli_fetch_object($resultado))
     {   
+        $arreglo_endosos=[];
         $conta=$conta+1;
     //$resultado contiene propuestas, a cada una de estas líneas hay que asignar un array con los ítem asociados
         //echo "primera query -> nro_propuesta: ".$row->numero_poliza."<br>";
@@ -31,35 +31,51 @@ $resultado=mysqli_query($link, $sql);
         $resultado_items=mysqli_query($link, "select a.numero_poliza, a.numero_item, a.id as id_item, a.materia_asegurada, a.patente_ubicacion, a.cobertura , a.deducible, CONCAT_WS(' ',FORMAT(tasa_afecta, 2, 'de_DE'),'%') AS tasa_afecta, CONCAT_WS(' ',FORMAT(tasa_exenta, 2, 'de_DE'),'%') AS tasa_exenta, CONCAT_WS(' ',b.moneda_poliza,FORMAT(prima_afecta, 2, 'de_DE')) AS prima_afecta, CONCAT_WS(' ',b.moneda_poliza,FORMAT(prima_exenta, 2, 'de_DE')) AS prima_exenta, CONCAT_WS(' ',b.moneda_poliza,FORMAT(prima_bruta_anual, 2, 'de_DE')) AS prima_bruta, CONCAT_WS(' ',b.moneda_poliza,FORMAT(prima_neta, 2, 'de_DE')) AS prima_neta, a.monto_asegurado, if(a.venc_gtia='0000-00-00','',a.venc_gtia) as venc_gtia, CONCAT_WS(' ',c.nombre_cliente,  c.apellido_paterno,  c.apellido_materno) as nom_clienteA, CONCAT_WS('-',c.rut_sin_dv, c.dv) as rut_clienteA,c.telefono as telefonoA, c.correo as correoA from items as a left join clientes as c  on a.rut_asegurado=c.rut_sin_dv and c.rut_sin_dv is not null left join polizas_2 as b on a.numero_poliza=b.numero_poliza where a.numero_poliza='".$row->numero_poliza."';");
             $items_array=array("total_items"=>& $fila->contador);
             if (!$cant_items=="0"){
-        while($indice=mysqli_fetch_object($resultado_items)){
-            //echo "tercera query -> nropropuesta: ".$row->numero_poliza."- ítem nro: ".$indice->id_item."<br>";
-            
-            $contador_contactos=$contador_contactos+1;
-            array_push($items, array(
-                "numero_item" =>& $indice->numero_item,
-                "materia_asegurada" =>& $indice->materia_asegurada,
-                "patente_ubicacion" =>& $indice->patente_ubicacion,
-                "cobertura" =>& $indice->cobertura,
-                "deducible" =>& $indice->deducible,
-                "tasa_afecta" =>& $indice->tasa_afecta,
-                "tasa_exenta" =>& $indice->tasa_exenta,
-                "prima_afecta" =>& $indice->prima_afecta,
-                "prima_exenta" =>& $indice->prima_exenta,
-                "prima_neta" =>& $indice->prima_neta,
-                "prima_bruta" =>& $indice->prima_bruta,
-                "nom_clienteA" =>& $indice->nom_clienteA,
-                "rut_clienteA" =>& $indice->rut_clienteA,
-                "telefonoA" =>& $indice->telefonoA,
-                "correoA" =>& $indice->correoA,
-                "venc_gtia" =>& $indice->venc_gtia,
-                "monto_asegurado" =>& $indice->monto_asegurado
-                ));
-            
-        }
-
+                while($indice=mysqli_fetch_object($resultado_items)){
+                    //echo "tercera query -> nropropuesta: ".$row->numero_poliza."- ítem nro: ".$indice->id_item."<br>";
+                    
+                    $contador_contactos=$contador_contactos+1;
+                    array_push($items, array(
+                        "numero_item" =>& $indice->numero_item,
+                        "materia_asegurada" =>& $indice->materia_asegurada,
+                        "patente_ubicacion" =>& $indice->patente_ubicacion,
+                        "cobertura" =>& $indice->cobertura,
+                        "deducible" =>& $indice->deducible,
+                        "tasa_afecta" =>& $indice->tasa_afecta,
+                        "tasa_exenta" =>& $indice->tasa_exenta,
+                        "prima_afecta" =>& $indice->prima_afecta,
+                        "prima_exenta" =>& $indice->prima_exenta,
+                        "prima_neta" =>& $indice->prima_neta,
+                        "prima_bruta" =>& $indice->prima_bruta,
+                        "nom_clienteA" =>& $indice->nom_clienteA,
+                        "rut_clienteA" =>& $indice->rut_clienteA,
+                        "telefonoA" =>& $indice->telefonoA,
+                        "correoA" =>& $indice->correoA,
+                        "venc_gtia" =>& $indice->venc_gtia,
+                        "monto_asegurado" =>& $indice->monto_asegurado
+                        ));
+                }
             }
-        
         }
+    $nro_endosos=$row->contador_endosos;
+    if (!$nro_endosos=="0"){
+        $nro_endosos=0;
+        $resultado_contador_endosos=mysqli_query($link, "SELECT e.numero_endoso, e.fecha_ingreso_endoso, e.fecha_prorroga, e.vigencia_inicial, e.vigencia_final, e.tipo_endoso, e.descripcion_endoso, e.dice, e.debe_decir, e.comentario_endoso, e.monto_asegurado_endoso, e.tasa_afecta_endoso, e.tasa_exenta_endoso, e.prima_neta_exenta, e.prima_neta_afecta, e.IVA, e.prima_total FROM endosos as e where e.id_poliza='".$row->id_poliza."';");
+        while($endosos=mysqli_fetch_object($resultado_contador_endosos)){
+            array_push($arreglo_endosos, array(
+                "numero_endoso" =>& $endosos->numero_endoso,
+                "tipo_endoso" =>& $endosos->tipo_endoso,
+                "descripcion_endoso" =>& $endosos->descripcion_endoso,
+                "dice" =>& $endosos->dice,
+                "debe_decir" =>& $endosos->debe_decir,
+                "vigencia_inicial" =>& $endosos->vigencia_inicial,
+                "vigencia_final" =>& $endosos->vigencia_final,
+                "fecha_ingreso_endoso" =>& $endosos->fecha_ingreso_endoso,
+                "fecha_prorroga" =>& $endosos->fecha_prorroga
+                ));
+            $nro_endosos+=1;
+        }
+    }
     if ($conta==1){
       $codigo.= json_encode(array_merge(array(
         "numero_poliza" =>& $row->numero_poliza,          //1
@@ -84,7 +100,9 @@ $resultado=mysqli_query($link, $sql);
         "id_poliza"=>& $row->id_poliza,                   //21
         "anomes_final" =>& $row->anomes_final,                  //22
         "anomes_inicial" =>& $row->anomes_inicial,              //23
-        "items" =>&$items                                       //24
+        "items" =>&$items,
+        "nro_endosos"=>&$nro_endosos,
+        "endosos"=>&$arreglo_endosos
         ),
         $items_array));
     } else {
@@ -111,7 +129,9 @@ $resultado=mysqli_query($link, $sql);
         "id_poliza"=>& $row->id_poliza,                   //21
         "anomes_final" =>& $row->anomes_final,                  //22
         "anomes_inicial" =>& $row->anomes_inicial,               //23
-        "items" =>&$items//23
+        "items" =>&$items,//23
+        "nro_endosos"=>&$nro_endosos,
+        "endosos"=>&$arreglo_endosos
         ),
         $items_array))
         ;
