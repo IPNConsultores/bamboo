144472066f828a8f274321d2b272aeea586787fb
IPNConsultores <Cesar.nar.m@gmail.com>
2022-06-05 19:07:43 -0400

    Update creacion_propuesta_poliza.php
    ahora si q si

--- bamboo/creacion_propuesta_poliza.php
+++ bamboo/creacion_propuesta_poliza.php
@@ -1333,6 +1333,20 @@ console.log(orgn)
         switch (orgn) 
         {
           case 'aceptar_poliza': {
+            var validador = ('<?php echo json_encode($rut_completo_aseg); ?>').includes('<?php echo $rut_completo_prop; ?>');
+            console.log(validador);
+
+            if (validador=true) 
+            {
+                console.log("iguales");
+                document.getElementById("radio2_si").checked = true;
+                document.getElementById("radio2_no").checked = false;
+            }
+            else{
+              console.log("diferentes");
+                document.getElementById("radio2_si").checked = false;
+                document.getElementById("radio2_no").checked = true;
+            }
             document.getElementById("informacion_poliza").style.display = "flex";
             document.getElementById("informacion_poliza").disabled = false;
             document.getElementById("nro_poliza").required = true;
@@ -1340,7 +1354,11 @@ console.log(orgn)
             break;  
           }
           case 'actualiza_propuesta':{
-            if ('<?php echo $rut_completo_prop; ?>' == '<?php echo $rut_completo_aseg; ?>') 
+
+            var validador = ('<?php echo json_encode($rut_completo_aseg); ?>').includes('<?php echo $rut_completo_prop; ?>');
+            console.log(validador);
+
+            if (validador=true) 
             {
                 console.log("iguales");
                 document.getElementById("radio2_si").checked = true;
@@ -1466,7 +1484,11 @@ console.log(orgn)
             break;
           }
           case 'crear_poliza':{
-            if ('<?php echo $rut_completo_prop; ?>' == '<?php echo $rut_completo_aseg; ?>') 
+
+            var validador = ('<?php echo json_encode($rut_completo_aseg); ?>').includes('<?php echo $rut_completo_prop; ?>');
+            console.log(validador);
+
+            if (validador=true) 
             {
                 document.getElementById("radio2_si").checked = true;
                 document.getElementById("radio2_no").checked = false;
@@ -1550,6 +1572,7 @@ console.log(orgn)
             break;
           }
           case 'modifica_poliza':{
+            
             var validador = ('<?php echo json_encode($rut_completo_aseg); ?>').includes('<?php echo $rut_completo_prop; ?>');
             console.log(validador);
 
