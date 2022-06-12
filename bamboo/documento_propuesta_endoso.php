800203a6d86cb9229b40b4c0390c974169893572
IPNConsultores <Cesar.nar.m@gmail.com>
2022-06-12 16:03:46 -0400

    Update documento_propuesta_endoso.php
    incorporar numero poliza en documento de endoso

--- bamboo/documento_propuesta_endoso.php
+++ bamboo/documento_propuesta_endoso.php
@@ -3,20 +3,19 @@ if ( !isset( $_SESSION ) ) {
   session_start();
 }
 
-/*
-$_SERVER[ "REQUEST_METHOD" ] = "POST";
-$_POST["accion"] = 'generar_documento';
-$_POST["numero_propuesta"]='E000012';
-*/
+
+//$_SERVER[ "REQUEST_METHOD" ] = "POST";
+//$_POST["accion"] = 'generar_documento';
+//$_POST["numero_propuesta"]='E000012';
+
     if ($_SERVER[ "REQUEST_METHOD" ] == "POST" and $_POST["accion"] == 'generar_documento')
     {
     
       require_once "/home/gestio10/public_html/backend/config.php";
       mysqli_set_charset( $link, 'utf8' );
       mysqli_select_db( $link, 'gestio10_asesori1_bamboo' );
-      $query = "select a.descripcion_endoso, numero_propuesta_endoso as numero_propuesta,moneda_poliza_endoso, a.compania, a.ramo, a.rut_proponente, a.dv_proponente, b.nombre_cliente, b.telefono, b.correo, b.direccion_personal, b.direccion_laboral,DATE_FORMAT(fecha_ingreso_endoso,'%d-%m-%Y') as fecha_propuesta , DATE_FORMAT(vigencia_inicial,'%d-%m-%Y') as vigencia_inicial, DATE_FORMAT(vigencia_final,'%d-%m-%Y') as vigencia_final, CONCAT(DATEDIFF(vigencia_final,vigencia_inicial),' días') as plazo_vigencia,DATE_FORMAT(fecha_prorroga,'%d-%m-%Y') as fecha_prorroga,  CONCAT_WS(' ',FORMAT(tasa_afecta_endoso, 2, 'de_DE'),'%') as tasa_afecta ,CONCAT_WS(' ',FORMAT(tasa_exenta_endoso, 2, 'de_DE'),'%')as tasa_exenta, CONCAT_WS(' ',FORMAT(prima_neta_afecta, 2, 'de_DE')) as prima_afecta,CONCAT_WS(' ',FORMAT(prima_neta_exenta, 2, 'de_DE')) as prima_exenta, CONCAT_WS(' ',FORMAT(IVA, 2, 'de_DE')) as prima_afecta_iva, CONCAT_WS(' ',FORMAT(prima_total, 2, 'de_DE')) as prima_bruta_anual, 
-            a.comentario_endoso, a.debe_decir, a.dice, a.monto_asegurado_endoso, a.tipo_endoso, a.numero_poliza, count(c.id) as numero_items
-            from propuesta_endosos as a left join clientes as b on a.rut_proponente=b.rut_sin_dv left join items as c on a.numero_poliza=c.numero_poliza where a.numero_propuesta_endoso='".$_POST["numero_propuesta"]."'";
+      $query = "select a.numero_poliza as numero_poliza,a.descripcion_endoso, numero_propuesta_endoso as numero_propuesta,moneda_poliza_endoso, a.compania, a.ramo, a.rut_proponente, a.dv_proponente, b.nombre_cliente, b.telefono, b.correo, b.direccion_personal, b.direccion_laboral,DATE_FORMAT(fecha_ingreso_endoso,'%d-%m-%Y') as fecha_propuesta , DATE_FORMAT(vigencia_inicial,'%d-%m-%Y') as vigencia_inicial, DATE_FORMAT(vigencia_final,'%d-%m-%Y') as vigencia_final, CONCAT(DATEDIFF(vigencia_final,vigencia_inicial),' días') as plazo_vigencia,DATE_FORMAT(fecha_prorroga,'%d-%m-%Y') as fecha_prorroga,  CONCAT_WS(' ',FORMAT(tasa_afecta_endoso, 2, 'de_DE'),'%') as tasa_afecta ,CONCAT_WS(' ',FORMAT(tasa_exenta_endoso, 2, 'de_DE'),'%')as tasa_exenta, CONCAT_WS(' ',FORMAT(prima_neta_afecta, 2, 'de_DE')) as prima_afecta,CONCAT_WS(' ',FORMAT(prima_neta_exenta, 2, 'de_DE')) as prima_exenta, CONCAT_WS(' ',FORMAT(IVA, 2, 'de_DE')) as prima_afecta_iva, CONCAT_WS(' ',FORMAT(prima_total, 2, 'de_DE')) as prima_bruta_anual, a.comentario_endoso, a.debe_decir, a.dice, a.monto_asegurado_endoso, a.tipo_endoso, a.numero_poliza, count(c.id) as numero_items
+                from propuesta_endosos as a left join clientes as b on a.rut_proponente=b.rut_sin_dv left join items as c on a.numero_poliza=c.numero_poliza where a.numero_propuesta_endoso='".$_POST["numero_propuesta"]."'";
       $resultado = mysqli_query( $link, $query );
       While( $row = mysqli_fetch_object( $resultado ) ) {
         $comentario_endoso = str_replace( "\r\n", "<br/>", $row->comentario_endoso );
@@ -32,6 +31,7 @@ $_POST["numero_propuesta"]='E000012';
         $prima_afecta_iva = $row->prima_afecta_iva;
         $prima_bruta = $row->prima_bruta_anual;
         $monto_aseg = $row->monto_asegurado_endoso;
+        $numero_poliza = $row -> numero_poliza;
         
         $descripcion_endoso=str_replace( "\r\n", "<br/>", $row->descripcion_endoso);
         $rut_prop = $row->rut_proponente;
@@ -104,6 +104,15 @@ $_POST["numero_propuesta"]='E000012';
                     
                 </div>
                  <div class="col-4" style="border-style :solid; border-color: grey; border-width: 2px; border-bottom-width:1px;border-left-width:1px;  ">
+                    <div class= "row align-items-center">
+                        <div class="col" style="background-color:#f5f5f5;border-style :solid; border-color: grey; border-width: 0px; border-top-width:0px; border-right-width: 0px;border-left-width: 0px;">
+                                <label>Número de Póliza:</label> 
+                        </div>
+                         <div class="col" contenteditable="true">       
+                                <label id="numero_poliza"></label>
+                                <br>
+                    </div>
+                    </div>
                     <div class= "row align-items-center">
                         <div class="col" style="background-color:#f5f5f5;border-style :solid; border-color: grey; border-width: 0px; border-top-width:0px; border-right-width: 0px;border-left-width: 0px;">
                                 <label>Propuesta Endoso:</label> 
@@ -554,7 +563,7 @@ $_POST["numero_propuesta"]='E000012';
 document.addEventListener("DOMContentLoaded", function(event) {
 //pobla información desde BBDD
     //  console.log('Cargando información');
-
+    document.getElementById("numero_poliza").innerHTML = '<?php echo $numero_poliza; ?>';
     document.getElementById("nro_propuesta").innerHTML = '<?php echo $nro_propuesta; ?>';
     document.getElementById("rut_proponente").innerHTML = '<?php echo $rut_completo_prop; ?>';
     document.getElementById("nombre_proponente").innerHTML = '<?php echo $nombre_proponente; ?>';
