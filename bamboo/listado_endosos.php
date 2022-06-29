5d02a394867d10dd8d35e86388c180a188fcbe56
H4tt3r <fabarca212@gmail.com>
2022-06-28 22:13:15 -0400

    Update listado_endosos.php

--- bamboo/listado_endosos.php
+++ bamboo/listado_endosos.php
@@ -276,7 +276,7 @@ function format_endoso(d) {
         '</tr>' +    
         '<tr><td VALIGN=TOP>Acciones</td>' +
         '<td>' +
-        '<button title="Editar Endoso" type="button" id=' + d.numero_endoso + ' name="actualizar_endoso" onclick="botones(this.id, this.name, \'endoso\')"><i class="fas fa-edit"></i></button><a> </a>' +
+        '<button title="Editar Endoso" type="button" id=' + d.numero_endoso + ' name="actualiza_endoso" onclick="botones(this.id, this.name, \'endoso\')"><i class="fas fa-edit"></i></button><a> </a>' +
         '<button title="Generar documento" type="button" id=' + d.numero_propuesta_endoso + ' name="generar_documento" onclick="botones(this.id, this.name, \'endoso\')"><i class="fa fa-file-pdf-o"></i></button><a> </a>' +
         '<button title="Buscar información asociada" type="button" id="' + d.numero_endoso + '" name="info" onclick="botones(this.id, this.name, \'endoso\')"><i class="fas fa-search"></i></button><a> </a>' +
         '</td>' +
@@ -326,7 +326,7 @@ function botones(id, accion, base) {
             }, 'post');
             break;
         }
-        case "actualizar_endoso": {
+        case "actualiza_endoso": {
             $.redirect('/bamboo/creacion_propuesta_endoso.php', {
                 'numero_endoso': id,
                 'accion': accion
